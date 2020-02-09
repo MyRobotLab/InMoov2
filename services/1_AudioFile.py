fb5e1a460f38b42c98fadbe0229cc117924a2374
Langevin Gael <hairygael@gmail.com>
2020-02-09 23:54:46 +0100

    Update 1_AudioFile.py

--- services/1_AudioFile.py
+++ services/1_AudioFile.py
@@ -17,8 +17,8 @@ MyMusicPath=ThisServicePartConfig.get('AUDIO', 'MyMusicPath')
 
 #for noworky
 log.info("MyMusicPath : "+str(MyMusicPath))
-musicpath = MyMusicPath
-
+musicpath = MyMusicPath 
+import os
 
 #startup sound
 AudioPlayer = Runtime.createAndStart("AudioPlayer", "AudioFile")
@@ -72,45 +72,110 @@ def AudioPlay(file):
   AudioPlayer.playFile(file,False)
       
 
-if StartupSound:AudioPlayer.playFileBlocking(RuningFolder+'/system/sounds/startupsound.mp3')
+
 python.subscribe(AudioPlayer.getName(),"publishAudioStart")
 python.subscribe(AudioPlayer.getName(),"publishAudioEnd")
+if StartupSound:AudioPlayer.playFile(RuningFolder+'/system/sounds/startupsound.mp3', False)
+sleep(2)
 
-# play a song from your music directory
-def play():
+global musiconoff
+global cpt
+musiconoff = 0
+cpt = 0
+
+ 
+# Function to create a list of mp3 files
+def fileList(musicpath):
+    global matches
+    global element
+    matches = []
+    for root, dirs, element in os.walk(musicpath):
+        for mp3 in element:
+            #if mp3.endswith(('.mp3', '.m4a', '.wav')):
+            if mp3.endswith(('.mp3', '.wav')):  
+                #print root+'/'+str(file)
+                #print mp3
+                matches.append(os.path.join(root+'/'+str(mp3)))
+   
+    return matches
+
+def playRandomMusic():
+      global musiconoff
+      fileList(musicpath)
+      musiconoff = 1
+      global matches
+      global element
+      global cpt
+      print("Loaded: " + str(len(matches)) + " files. " + str(len(element)) + " files for random")
+      cpt = len(matches)
+      print (cpt)
+ 
+      random.shuffle(matches)
+      
+      for i in range(0,len(element)+1):
+        print(matches[i])
+        sleep(1)
+        AudioPlayer.playFile((matches[i]) , False)
+        ear.startListening()
+        sleep(1)
+        ear.setAutoListen(setAutoListen)
+       
+
+def playMusic():
     global musiconoff
     musiconoff = 1
-    #musicpath = (RuningFolder+"/system/sounds/")
-    files = os.listdir(musicpath)
-    song=random.choice(files)
-    sleep(3)
-    #i01.mouth.speakBlocking("playing song" + str(song))
-    AudioPlayer.playFile(musicpath + str(song) , False)
-    print("playing song:" + str(song))
+    # Select one of the mp3 files from the list at random
+    mp3 = random.choice(fileList(musicpath))
+    print mp3
+    # Play the file
     sleep(1)
+    AudioPlayer.playFile((mp3) , False)
     ear.startListening()
-    ear.setAutoListen(True)
+    sleep(1)
+    ear.setAutoListen(setAutoListen)
 
-def nextMusic():
-    if musiconoff == 1:
-        AudioPlayer.stop()
-        print "next music" 
-        play()
 
 # pause method 
-def pause():
+def pauseMusic():
+    global musiconoff
     if musiconoff == 1:
       AudioPlayer.pause()
-      print "set music on pause"
+      print "Pause music"
 
 # resume method 
-def resume():
+def resumeMusic():
+    global musiconoff
     if musiconoff == 1:
       AudioPlayer.resume()
-      print "resuming music"
-        
-# stopped method is called when at the end of an audio file
-def stop():
+      print "Resume song"
+
+# stop the music
+def stopMusic():
+    global musiconoff
+    global cpt
+    #print (cpt)
     if musiconoff == 1:
         AudioPlayer.stop()
-        print "stop playing"
+        print "Stop music"
+        if cpt > 1:
+            i = -2
+            while i < cpt :   
+                #print (i)
+                AudioPlayer.stop()
+                i = i+1
+                sleep(0.2)
+                ear.startListening()
+                sleep(1)
+                ear.setAutoListen(setAutoListen)
+            cpt = 0
+            musiconoff = 0
+        
+def nextMusic():
+    global musiconoff
+    if musiconoff == 1:
+        AudioPlayer.stop()
+        play()
+        sleep(1)
+        ear.startListening()
+        sleep(1)
+        ear.setAutoListen(setAutoListen)
