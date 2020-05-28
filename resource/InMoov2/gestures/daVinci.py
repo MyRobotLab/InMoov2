93b31932f064a047c6aa4a0e91d6852fff74a2dc
Kevin Watters <kwatters@kmwllc.com>
2020-05-28 17:06:31 -0400

    Update daVinci.py
    example of turning off autoDisable for davinci omoplates and restoring their states back after.

--- resource/InMoov2/gestures/daVinci.py
+++ resource/InMoov2/gestures/daVinci.py
@@ -1,5 +1,11 @@
 def daVinci():
   i01.startedGesture()
+  # get the current value of auto disable
+  leftPreviousAutoDisableValue = i01.leftArm.omplate.getAutoDisable()
+  rightPreviousAutoDisableValue = i01.rightArm.omplate.getAutoDisable()
+  # turn off auto disable for this gesture
+  i01.leftArm.omplate.setAutoDisable(False)
+  i01.rightArm.omplate.setAutoDisable(False)
   i01.setHandVelocity("left", -1, -1, -1, -1, -1, 22.0)
   i01.setHandVelocity("right", -1, -1, -1, -1, -1, 22.0)
   i01.setArmVelocity("left", 36, 36, 36, 36)
@@ -11,5 +17,8 @@ def daVinci():
   i01.moveHand("left",50,40,30,20,10,47)
   i01.moveHand("right",50,40,30,20,10,137)
   sleep(5)
+  # restore the auto disable value after the gesture is done.
+  i01.leftArm.omplate.setAutoDisable(leftPreviousAutoDisableValue)
+  i01.rightArm.omplate.setAutoDisable(rightPreviousAutoDisableValue)
   i01.finishedGesture()
 
