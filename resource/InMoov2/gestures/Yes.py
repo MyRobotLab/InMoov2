5c58d8440bcf4fedc448a9b3265da896ded5287e
Langevin Gael <hairygael@gmail.com>
2021-04-29 23:40:36 +0200

    Add files via upload

--- resource/InMoov2/gestures/Yes.py
+++ resource/InMoov2/gestures/Yes.py
@@ -1,7 +1,7 @@
 def Yes():
   if isHeadActivated==1:
     i01.startedGesture()
-    i01.setHeadVelocity(45,45,45)
+    i01.setHeadSpeed(45,45,45)
     i01.moveHeadBlocking(130,90)
     i01.moveHeadBlocking(50,93)
     i01.moveHeadBlocking(120,88)
