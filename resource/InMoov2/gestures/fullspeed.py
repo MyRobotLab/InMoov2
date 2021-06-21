4ecd03f2c873d389f4472108f0cadce5c860151a
Langevin Gael <hairygael@gmail.com>
2021-06-21 19:54:24 +0200

    Update fullspeed.py

--- resource/InMoov2/gestures/fullspeed.py
+++ resource/InMoov2/gestures/fullspeed.py
@@ -4,24 +4,24 @@ def fullspeed():
     sleep(1)
     i01.setNeopixelAnimation("Ironman", 0, 0, 255, 1)
   if isRightHandActivated:
-    i01.setHandSpeed("right", 100.0, 100.0, 100.0, 100.0, 100.0, 100.0)
+    i01.setHandSpeed("right", 500.0, 500.0, 500.0, 500.0, 500.0, 500.0)
       
   if isLeftHandActivated:
-    i01.setHandSpeed("left", 100.0, 100.0, 100.0, 100.0, 100.0, 100.0)
+    i01.setHandSpeed("left", 500.0, 500.0, 500.0, 500.0, 500.0, 500.0)
       
   if isRightArmActivated:
-    i01.setArmSpeed("right", 100.0, 100.0, 100.0, 100.0)
+    i01.setArmSpeed("right", 500.0, 500.0, 500.0, 500.0)
     
   if isLeftArmActivated:
-    i01.setArmSpeed("left", 100.0, 100.0, 100.0, 100.0)
+    i01.setArmSpeed("left", 500.0, 500.0, 500.0, 500.0)
   
   if isHeadActivated:
-    i01.setHeadSpeed(100.0, 100.0, 100.0, 100.0, 100.0, 100.0)
+    i01.setHeadSpeed(500.0, 500.0, 500.0, 500.0, 500.0, 500.0)
   
   if isTorsoActivated:
-    i01.setTorsoSpeed(100.0, 100.0, 100.0)
+    i01.setTorsoSpeed(500.0, 500.0, 500.0)
       
   if isEyeLidsActivated:
-    i01.head.eyelidLeft.setSpeed(100.0)
-    i01.head.eyelidRight.setSpeed(100.0)
+    i01.head.eyelidLeft.setSpeed(500.0)
+    i01.head.eyelidRight.setSpeed(500.0)
 
