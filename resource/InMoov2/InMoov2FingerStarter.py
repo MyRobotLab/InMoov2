225fe627ee9a62ca92f08008f5d9eaeec7af0703
Langevin Gael <hairygael@gmail.com>
2021-07-29 13:45:09 +0200

    Update and rename InMoovFingerStarter.py to InMoov2FingerStarter.py

--- resource/InMoov2/InMoov2FingerStarter.py
+++ resource/InMoov2/InMoov2FingerStarter.py
@@ -1,42 +1,35 @@
-#file : InMoov.minimalFingerStarter.py
+#file : InMoov2.minimalFingerStarter.py
 # MRL version >= Nixie
 # this script is provided as a basic guide
 # most parts can be run by uncommenting them
-# InMoov now can be started in modular pieces through the skeleton.config
+# InMoov now can be started in modular pieces through the UI and configs
 # although this script is very short you can still
 # do voice control of a FingerStarter or hand
 # It uses WebkitSpeechRecognition, so you need to use Chrome as your default browser for this script to work
+# Make sure chrome can use your microphone
 
 # Change to the port that you use
 rightPort = "COM9"
 ##############
 #to tweak the default voice
 Voice="Mark" #Male US voice 
-#Voice="cmu-slt-hsmm" #Default female for MarySpeech
+#Voice="Poppy" #Female for MarySpeech
 mouth = Runtime.createAndStart("i01.mouth", "MarySpeech")
 #mouth.installComponentsAcceptLicense(Voice)
 mouth.setVoice(Voice)
 ##############
-# starting InMoov service
-i01 = Runtime.create("i01", "InMoov")
+# starting InMoov2 service
+i01 = Runtime.create("i01", "InMoov2")
 #Force Arduino to connect (fix Todo)
 right = Runtime.createAndStart("i01.right", "Arduino")
 right.connect(rightPort)
 ##############
 # Starting parts
 i01.startEar()
-# Start the webgui service without starting the browser
-webgui = Runtime.create("WebGui","WebGui")
-webgui.autoStartBrowser(False)
-webgui.startService()
-# Then start the browsers and show the WebkitSpeechRecognition service named i01.ear
-webgui.startBrowser("http://localhost:8888/#/service/i01.ear")
-# As an alternative you can use the line below to show all services in the browser. In that case you should comment out all lines above that starts with webgui. 
-# webgui = Runtime.createAndStart("webgui","WebGui")
 ##############
 i01.startMouth()
 ##############
-rightHand = Runtime.create("i01.rightHand","InMoovHand")
+rightHand = Runtime.create("i01.rightHand","InMoov2Hand")
 # Tweaking defaults settings of right hand
 
 # Mapping by setting your servo limits
@@ -44,15 +37,13 @@ rightHand.index.map(0,180,42,160)
 # Rest position
 rightHand.index.setRest(0)
 ##############
-i01 = Runtime.start("i01","InMoov")
+i01 = Runtime.start("i01","InMoov2")
 ##############
 i01.startRightHand(rightPort)
 #i01.rightHand.setAutoEnable(True)
 ##############
 # Verbal commands
 ear = i01.ear
-#always listen
-#ear.setAutoListen(True)
 
 ear.addCommand("attach your finger", "i01.rightHand.index", "enable")
 ear.addCommand("disconnect your finger", "i01.rightHand.index", "disable")
@@ -72,16 +63,16 @@ ear.addCommand("voice control", ear.getName(), "clearLock")
 ear.startListening()
 
 def fingeropen():
-  i01.rightHand.index.setVelocity(20)## Low velocity
+  i01.rightHand.index.setSpeed(20)## Low speed
   i01.moveHand("right",0,0,0,0,0,0)## Thumb,index,majeure,ringfinger,pinky,wrist
   i01.mouth.speak("ok I open my finger")
 
 def fingerclose():
-  i01.rightHand.index.setVelocity(50)## Medium velocity
+  i01.rightHand.index.setSpeed(100)## Medium speed
   i01.moveHand("right",180,180,180,180,180,90)
   i01.mouth.speak("my finger is closed")
 
 def fingermiddle():
-  i01.rightHand.index.setVelocity(-1)## Maximum velocity
+  i01.rightHand.index.setSpeed(500)## Maximum speed
   i01.moveHand("right",90,90,90,90,90,90)
   i01.mouth.speak("ok you have my attention")
