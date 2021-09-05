8056216703fd3e76872fc31dea56e28b5e894c73
Langevin Gael <hairygael@gmail.com>
2021-09-05 22:22:52 +0200

    Update find_image.py

--- resource/InMoov2/gestures/find_image.py
+++ resource/InMoov2/gestures/find_image.py
@@ -1,12 +1,12 @@
 def find_image(image):
-  print('please implement by using a image search service')
+#  print('please implement by using a image search service')
 #  RetourServer=Parse("http://www.myai.cloud/version.html")
-#  try:
-#    image = image.decode( "utf8" )
-#  except: 
-#    pass
-#  a = Parse("http://www.myai.cloud/bot1.php?type=pic&pic="+urllib2.quote(image).replace(" ", "%20"))
-#  
-#  display(a)
-#  print "http://www.myai.cloud/bot1.php?type=pic&pic="+urllib2.quote(image).replace(" ", "%20")
+  try:
+    image = image.decode( "utf8" )
+  except: 
+    pass
+  a = Parse("https://www.google.com/search?tbm=isch&q="+urllib2.quote(image).replace(" ", "%20"))
+  
+  i01.displayFullScreen(a)
+  print "https://www.google.com/search?tbm=isch&q="+urllib2.quote(image).replace(" ", "%20")
 #Light(1,1,1)
