23e9a040040dded39583cdee91a55c181dce67ca
Langevin Gael <hairygael@gmail.com>
2021-09-07 00:53:56 +0200

    Update find_image.py

--- resource/InMoov2/gestures/find_image.py
+++ resource/InMoov2/gestures/find_image.py
@@ -1,12 +1,19 @@
 def find_image(image):
-#  print('please implement by using a image search service')
-#  RetourServer=Parse("http://www.myai.cloud/version.html")
-  try:
-    image = image.decode( "utf8" )
-  except: 
-    pass
-  a = Parse("https://www.google.com/search?tbm=isch&q="+urllib2.quote(image).replace(" ", "%20"))
-  
-  i01.displayFullScreen(a)
-  print "https://www.google.com/search?tbm=isch&q="+urllib2.quote(image).replace(" ", "%20")
-#Light(1,1,1)
+  if isChatBotActivated:
+    python.subscribe('i01.chatBot', 'publishResults')
+    try:
+      image = image.decode( 'utf8' )
+    except: 
+      pass
+    a = i01_chatBot.search(image)
+    #a = i01_chatBot.search(+urllib2.quote(image).replace(" ", "%20"))
+    display(a)
+  else:
+    google = Runtime.start('google','GoogleSearch')
+    python.subscribe('google', 'publishResults')
+    try:
+      image = image.decode( 'utf8' )
+    except: 
+      pass
+    a = google.search(image)  
+    display(a)
