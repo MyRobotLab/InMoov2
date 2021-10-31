4fa07656dc9af0b9a50e67d327fa8810bfca2ed2
Langevin Gael <hairygael@gmail.com>
2021-10-31 10:07:44 +0100

    fix for shutdown

--- resource/InMoov2/life/shutdownComplete.py
+++ resource/InMoov2/life/shutdownComplete.py
@@ -1,6 +1,7 @@
 python.subscribe(runtime,"shutdown")
 
 def shutdownComplete():
+  i01.speakBlocking(i01.localize("SHUTDOWNCOMPLETE"))
   runtime.execute("cmd.exe","/c","shutdown.exe /s /t 30 /f")
   shutdown()
   
