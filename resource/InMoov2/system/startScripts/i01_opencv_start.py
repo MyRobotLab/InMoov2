fe8dec2453fa135cc9abd839e6560dd357ed2a0f
Langevin Gael <hairygael@gmail.com>
2021-11-12 15:39:54 +0100

    Update i01_opencv_start.py

--- resource/InMoov2/system/startScripts/i01_opencv_start.py
+++ resource/InMoov2/system/startScripts/i01_opencv_start.py
@@ -10,5 +10,7 @@
 i01_opencv = Runtime.start("i01.opencv","OpenCV")
 #i01_opencv.setGrabberType("org.bytedeco.javacv.OpenCVFrameGrabber")
 i01_opencv.setGrabberType("OpenCV")
+i01_opencv.nativeViewer=False
+i01_opencv.webViewer=True
 i01.speakBlocking(i01.localize("STARTINGOPENCV"))
 isOpenCVActivated=True
