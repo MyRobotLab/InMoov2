209944509e4df8ac8fbc23ed40e2f268d6a8573b
Langevin Gael <hairygael@gmail.com>
2021-11-19 23:28:20 +0100

    Create i01.basic.py

---
+++ resource/InMoov2/config/InMoov2_Basic/i01.basic.py
@@ -0,0 +1,22 @@
+#################################################################
+## InMoov2 CONFIG TYPES
+#################################################################
+InMoov2_FingerStarter = 'data/config/InMoov2_FingerStarter/'
+InMoov2_Full = 'data/config/InMoov2_Full/'
+InMoov2_Head = 'data/config/InMoov2_Head/'
+InMoov2_LeftHand = 'data/config/InMoov2_LeftHand/'
+InMoov2_LeftSide = 'data/config/InMoov2_LeftSide/'
+InMoov2_RightSide = 'data/config/InMoov2_RightSide/'
+#################################################################
+# Select your config type
+ConfigType = InMoov2_FingerStarter
+
+# Some basic behaviors
+DEBUG=False
+IsMute=False
+LoadingPicture=False
+StartupSound=False
+LaunchWebGui=True
+BetaVersion=False
+
+isRightHandActivated=True
