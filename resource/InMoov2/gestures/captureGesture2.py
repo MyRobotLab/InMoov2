e8085618aa2175b7a62de2617487c3e753075989
Langevin Gael <hairygael@gmail.com>
2022-01-05 11:10:08 +0100

    update audiofile

--- resource/InMoov2/gestures/captureGesture2.py
+++ resource/InMoov2/gestures/captureGesture2.py
@@ -1,3 +1,3 @@
 def captureGesture2():
-  AudioPlayer.playFile(RuningFolder+'/system/sounds/ShutterCapture.mp3')
+  i01.audioPlayer.playFile(RuningFolder+'/system/sounds/ShutterCapture.mp3')
   i01.captureGesture()
