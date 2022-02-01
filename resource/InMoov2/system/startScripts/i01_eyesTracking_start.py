3788bc9f0825e59f9a4c767ca8cbc051c0f74011
Langevin Gael <hairygael@gmail.com>
2022-02-01 23:32:02 +0100

    start and stop scripts head eyes tracking

---
+++ resource/InMoov2/system/startScripts/i01_eyesTracking_start.py
@@ -0,0 +1,35 @@
+#########################################
+# i01_eyesTracking_start.py
+# categories: inmoov2
+# more info @: http://myrobotlab.org/service/InMoov
+#########################################
+# uncomment for virtual hardware
+# Platform.setVirtual(True)
+
+# create a pid
+i01_eyesPid = Runtime.createAndStart("i01.eyesPid","Pid")
+#set Kp, kd, ki kp = gain, how strong it react kd = how fast it react ki= take care of the sum of errors (differences between target and actual value) in the time
+i01_eyesPid.setPid("i01.head.eyeX", 0.035, 0.01, 0.0)
+i01_eyesPid.setMode("i01.head.eyeX", 1)
+#set ControllerDirection means inverted = 1
+i01_eyesPid.setControllerDirection("i01.head.eyeX", 0)
+#set the range of the "correction"
+i01_eyesPid.setOutputRange("i01.head.eyeX", -5, 5)
+
+i01_eyesPid.setPid("i01.head.eyeY", 0.035, 0.01, 0.0)
+i01_eyesPid.setMode("i01.head.eyeY", 1)
+i01_eyesPid.setControllerDirection("i01.head.eyeY", 0)
+#set the range of the "correction"
+i01_eyesPid.setOutputRange("i01.head.eyeY", -5, 5)
+
+
+# create a tracking
+i01_eyesTracking = Runtime.start("i01.eyesTracking","Tracking")
+i01_eyesTracking.attachCv("i01.opencv")
+i01_eyesTracking.attachPan("i01.head.eyeX")
+i01_eyesTracking.attachTilt("i01.head.eyeY")
+i01_eyesTracking.attachPid("i01.eyesPid")
+i01_eyesTracking.enable()
+
+
+
---
+++ resource/InMoov2/system/startScripts/i01_headTracking_start.py
@@ -0,0 +1,35 @@
+#########################################
+# i01_headTracking_start.py
+# categories: inmoov2
+# more info @: http://myrobotlab.org/service/InMoov
+#########################################
+# uncomment for virtual hardware
+# Platform.setVirtual(True)
+
+# create a pid
+i01_headPid = Runtime.createAndStart("i01.headPid","Pid")
+#set Kp, kd, ki kp = gain, how strong it react kd = how fast it react ki= take care of the sum of errors (differences between target and actual value) in the time
+i01_headPid.setPid("i01.head.rothead", 0.035, 0.01, 0.0)
+i01_headPid.setMode("i01.head.rothead", 1)
+#set ControllerDirection means inverted = 1
+i01_headPid.setControllerDirection("i01.head.rothead", 0)
+#set the range of the "correction"
+i01_headPid.setOutputRange("i01.head.rothead", -5, 5)
+
+i01_headPid.setPid("i01.head.neck", 0.035, 0.01, 0.0)
+i01_headPid.setMode("i01.head.neck", 1)
+i01_headPid.setControllerDirection("i01.head.neck", 0)
+#set the range of the "correction"
+i01_headPid.setOutputRange("i01.head.neck", -5, 5)
+
+enablePid = True
+
+# create a tracking
+i01_headTracking = Runtime.start("i01.headTracking","Tracking")
+i01_headTracking.attachCv("i01.opencv")
+i01_headTracking.attachPan("i01.head.rothead")
+i01_headTracking.attachTilt("i01.head.neck")
+i01_headTracking.attachPid("i01.headPid")
+i01_headTracking.enable()
+
+enableTracking = True
