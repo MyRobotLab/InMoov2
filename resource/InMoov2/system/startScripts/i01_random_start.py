21276b00e29e18439dd30240bb78a7034aea44e7
Langevin Gael <hairygael@gmail.com>
2022-02-07 19:12:50 +0100

    fix random

--- resource/InMoov2/system/startScripts/i01_random_start.py
+++ resource/InMoov2/system/startScripts/i01_random_start.py
@@ -6,8 +6,8 @@
 # uncomment for virtual hardware
 
 moveHeadRandomize = Runtime.start('moveHeadRandomize','Random')
-if i01.isOpenCvActivated:
+if i01.isHeadActivated:
     if not i01_opencv.isCapturing():
-      moveHeadRandomize.addRandom(1000, 20000, 'i01.head.rothead', 'moveTo', random.intRange(50, 130))
-      moveHeadRandomize.addRandom(1000, 20000, 'i01.head.neck', 'moveTo', random.intRange(35, 165))
-      moveHeadRandomize.addRandom(1000, 20000, 'i01.head.rollNeck', 'moveTo', random.intRange(35, 165))
+      moveHeadRandomize.addRandom(1000, 20000, 'i01.head.rothead', 'moveTo', moveHeadRandomize.intRange(50, 130))
+      moveHeadRandomize.addRandom(1000, 20000, 'i01.head.neck', 'moveTo', moveHeadRandomize.intRange(35, 165))
+      moveHeadRandomize.addRandom(1000, 20000, 'i01.head.rollNeck', 'moveTo', moveHeadRandomize.intRange(35, 165))
