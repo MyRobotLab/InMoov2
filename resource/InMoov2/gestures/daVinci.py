e0f14ddd4fc4f3c39e44883f845ace1218b59a2c
Langevin Gael <hairygael@gmail.com>
2022-02-14 23:41:50 +0100

    some gesture fix

--- resource/InMoov2/gestures/daVinci.py
+++ resource/InMoov2/gestures/daVinci.py
@@ -1,11 +1,11 @@
 def daVinci():
   i01.startedGesture()
   # get the current value of auto disable
-  leftPreviousAutoDisableValue = i01.leftArm.omoplate.isAutoDisable()
-  rightPreviousAutoDisableValue = i01.rightArm.omoplate.isAutoDisable()
+  leftPreviousAutoDisableValue = i01_leftArm_omoplate.isAutoDisable()
+  rightPreviousAutoDisableValue = i01_rightArm_omoplate.isAutoDisable()
   # turn off auto disable for this gesture
-  i01.leftArm.omoplate.setAutoDisable(False)
-  i01.rightArm.omoplate.setAutoDisable(False)
+  i01_leftArm_omoplate.setAutoDisable(False)
+  i01_rightArm_omoplate.setAutoDisable(False)
   i01.setHandSpeed("left", 100.0, 100.0, 100.0, 100.0, 100.0, 22.0)
   i01.setHandSpeed("right", 100.0, 100.0, 100.0, 100.0, 100.0, 22.0)
   i01.setArmSpeed("left", 36, 36, 36, 36)
@@ -18,7 +18,7 @@ def daVinci():
   i01.moveHand("right",50,40,30,20,10,137)
   sleep(5)
   # restore the auto disable value after the gesture is done.
-  i01.leftArm.omoplate.setAutoDisable(leftPreviousAutoDisableValue)
-  i01.rightArm.omoplate.setAutoDisable(rightPreviousAutoDisableValue)
+  i01_leftArm_omoplate.setAutoDisable(leftPreviousAutoDisableValue)
+  i01_rightArm_omoplate.setAutoDisable(rightPreviousAutoDisableValue)
   i01.finishedGesture()
 
