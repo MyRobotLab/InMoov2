578696c5685e4e84f220ea2992d83171649cc660
Langevin Gael <hairygael@gmail.com>
2022-02-20 15:54:59 +0100

    syntax error

--- resource/InMoov2/gestures/stopTracking.py
+++ resource/InMoov2/gestures/stopTracking.py
@@ -1,5 +1,5 @@
 def stopTracking():
-  if runtime.isStarted("i01.opencv")
+  if runtime.isStarted("i01.opencv"):
     if (i01_opencv.isCapturing()):
       i01_opencv.stopTracking()
       i01_head_rollNeck.setAutoDisable(True)
