30a0f795ec41418ef950160e5e39a77d5ca92601
Langevin Gael <hairygael@gmail.com>
2022-02-27 19:17:49 +0100

    Update takePicture.py

--- resource/InMoov2/gestures/takePicture.py
+++ resource/InMoov2/gestures/takePicture.py
@@ -1,8 +1,43 @@
 def takePicture():
-  i01.cameraOn()
-  photoFileName = i01_opencv.recordFrame()
-  print photoFileName
-  i01_audioPlayer.playFile(RuningFolder+'/system/sounds/ShutterClik.mp3')
-  imagedisplay.display(photoFileName)
-  sleep(15)
-  imagedisplay.closeAll()
+    if runtime.isStarted('i01.opencv'):
+        photoFileName = i01_opencv.recordFrame()
+        print photoFileName
+        if runtime.isStarted('imageDisplay'):
+            imageDisplay = runtime.start("imageDisplay", "ImageDisplay")
+            #sleep(3)
+            i01_audioPlayer.playFile('resource/InMoov2/system/sounds/ShutterClik.mp3')
+            #imageDisplay.display(photoFileName)
+            imageDisplay.displayFullScreen(photoFileName)
+            sleep(15)
+            imageDisplay.closeAll()
+        else:
+            imageDisplay = runtime.start("imageDisplay", "ImageDisplay")
+            sleep(3)
+            i01_audioPlayer.playFile('resource/InMoov2/system/sounds/ShutterClik.mp3')
+            sleep(1)
+            #imageDisplay.display(photoFileName)
+            imageDisplay.displayFullScreen(photoFileName)
+            sleep(15)
+            imageDisplay.closeAll()
+    else:
+        i01.cameraOn()
+        if runtime.isStarted('imageDisplay'):
+            photoFileName = i01_opencv.recordFrame()
+            print photoFileName
+            i01_audioPlayer.playFile('resource/InMoov2/system/sounds/ShutterClik.mp3')
+            sleep(1)
+            #imageDisplay.display(photoFileName)
+            imageDisplay.displayFullScreen(photoFileName)
+            sleep(15)
+            imageDisplay.closeAll()
+        else:
+            imageDisplay = runtime.start("imageDisplay", "ImageDisplay")
+            sleep(5)
+            i01_audioPlayer.playFile('resource/InMoov2/system/sounds/ShutterClik.mp3')
+            sleep(2)
+            photoFileName = i01_opencv.recordFrame()
+            print photoFileName
+            sleep(1)
+            #imageDisplay.display(photoFileName)
+            imageDisplay.displayFullScreen(photoFileName)
+            sleep(15)
