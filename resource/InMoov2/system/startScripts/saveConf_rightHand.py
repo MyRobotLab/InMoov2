82cebfaa42c0345dfe9be79ec0c117d24ff4adeb
Langevin Gael <hairygael@gmail.com>
2022-03-04 18:06:18 +0100

    Update saveConf_rightHand.py

--- resource/InMoov2/system/startScripts/saveConf_rightHand.py
+++ resource/InMoov2/system/startScripts/saveConf_rightHand.py
@@ -31,18 +31,17 @@ def saveConfig(configFilename):
             s = Servo #+os.getcwd().replace('i01.rightHand.','')
             #configWriter.write("# Servo Name : " + s.getName(InMoov2Hand) + "\n")
             #configWriter.write("[SERVO_PIN]\n")
-            #configWriter.write("pin = " + s.getPin(InMoov2Hand) + "\n")
             #configWriter.write("# " + s.getName(InMoov2Hand) + " pin = " + s.getPin(InMoov2Hand) + "\n")
             configWriter.write(s.getName(InMoov2Hand) + " = " + s.getPin(InMoov2Hand) + "\n")
             #configWriter.write(s.getControllerName(InMoov2Hand) + "\n")
             #configWriter.write("rest = " + s.getRest(InMoov2Hand) + "\n")
             #for root, subdirs, files in os.walk(configPath+configFile):
                 #os.path.basename([0][1]).replace('i01.rightHand.','')
-    #configWriter.write("[CONTROLLER]\n")
-    #for controller in Runtime.getServices():
-        #if isinstance(controller, Controller):
-            #c = Controller
-            #configWriter.write(c.getControllerName(controller) + "\n")            
+    configWriter.write("[CONTROLLER]\n")
+    for i01_right in Runtime.getServices():
+        if isinstance(i01_right, Arduino):
+            a = Arduino
+            configWriter.write(a.getName(i01_right) + " = " + a.getPortName(i01_right) + "\n")           
     configWriter.write("finitum\n")
     configWriter.close()
 
