3bb1b1ac288269ad0948178ce11f8b7f5a3550f0
Langevin Gael <hairygael@gmail.com>
2022-03-16 21:01:38 +0100

    imageDisplay button

---
+++ resource/InMoov2/system/startScripts/imageDisplay_start.py
@@ -0,0 +1,9 @@
+#########################################
+# imageDisplay_start.py
+# categories: inmoov2
+# more info @: http://myrobotlab.org/service/InMoov
+#########################################
+# uncomment for virtual hardware
+# Platform.setVirtual(True)
+
+imageDisplay = Runtime.start('imageDisplay', 'ImageDisplay')
