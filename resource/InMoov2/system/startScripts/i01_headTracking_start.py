9e64062b466775438ebba2a8c96fcf1e093ad5ce
Langevin Gael <hairygael@gmail.com>
2022-04-03 18:20:50 +0200

    setPid higher

--- resource/InMoov2/system/startScripts/i01_headTracking_start.py
+++ resource/InMoov2/system/startScripts/i01_headTracking_start.py
@@ -9,20 +9,18 @@
 # create a pid
 i01_pid = Runtime.start("i01.pid","Pid")
 #set Kp, kd, ki kp = gain, how strong it react kd = how fast it react ki= take care of the sum of errors (differences between target and actual value) in the time
-i01_pid.setPid("i01.head.rothead", 0.035, 0.01, 0.0)
+i01_pid.setPid("i01.head.rothead", 35.0, 1.0, 0.0)
 i01_pid.setMode("i01.head.rothead", 1)
 #set ControllerDirection means inverted = 1
 i01_pid.setControllerDirection("i01.head.rothead", 0)
 #set the range of the "correction"
 i01_pid.setOutputRange("i01.head.rothead", -5, 5)
 
-i01_pid.setPid("i01.head.neck", 0.035, 0.01, 0.0)
+i01_pid.setPid("i01.head.neck", 35.0, 1.0, 0.0)
 i01_pid.setMode("i01.head.neck", 1)
 i01_pid.setControllerDirection("i01.head.neck", 0)
 #set the range of the "correction"
 i01_pid.setOutputRange("i01.head.neck", -5, 5)
-
-#enablePid = True
 
 # create a tracking
 i01_headTracking = Runtime.start("i01.headTracking","Tracking")
@@ -31,7 +29,5 @@ i01_headTracking.attachPan("i01.head.rothead")
 i01_headTracking.attachTilt("i01.head.neck")
 i01_headTracking.attachPid("i01.pid")
 i01_headTracking.enable()
-
-#enableTracking = True
 
 #i01.startPeer('headTracking')
