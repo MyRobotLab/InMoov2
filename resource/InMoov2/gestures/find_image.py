2e473b29d6f59a552708c4bd92a7fd484a266f09
Langevin Gael <hairygael@gmail.com>
2022-10-31 00:32:47 +0100

    Update find_image.py

--- resource/InMoov2/gestures/find_image.py
+++ resource/InMoov2/gestures/find_image.py
@@ -4,17 +4,6 @@ def find_image(image):
   chatBot_search.attach(imageDisplay)
   chatBot_search.search(image)
   print('print picture of', image)
-
-  if runtime.isStarted('i01.ChatBot'):
-    python.subscribe('i01.chatBot', 'publishResults')
-    try:
-      image = image.decode( 'utf8' )
-    except: 
-      pass
-      a = i01_chatBot.search(image)
-      #a = i01_chatBot.search(+urllib2.quote(image).replace(" ", "%20"))
-      imageDisplay.display(a)
-      sleep(3)
-      imageDisplay.closeAll()
-      i01.finishedGesture()
-      
+  sleep(3)
+  imageDisplay.closeAll()
+  
