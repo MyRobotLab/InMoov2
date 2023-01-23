84a0cf246681152a8a7ab2fe507482ad64843510
Langevin Gael <hairygael@gmail.com>
2023-01-23 21:19:56 +0100

    Update trackHumans.py

--- resource/InMoov2/gestures/trackHumans.py
+++ resource/InMoov2/gestures/trackHumans.py
@@ -4,6 +4,7 @@ def trackHumans():
     i01_head_rollNeck.moveToBlocking(90)
     i01.setHeadSpeed(500.0,500.0,500.0,500.0,500.0)
     i01_opencv.removeFilters()
+    i01.getHeadTracking()
     i01_headTracking.enable()
   else:
     i01.warn("i01.opencv needs to be started to start tracking!")
