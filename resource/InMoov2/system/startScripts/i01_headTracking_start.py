30b4db374a83dbfb35ea96ac1097e34d6e08ca2c
Langevin Gael <hairygael@gmail.com>
2023-01-23 20:47:29 +0100

    Update i01_headTracking_start.py

--- resource/InMoov2/system/startScripts/i01_headTracking_start.py
+++ resource/InMoov2/system/startScripts/i01_headTracking_start.py
@@ -7,7 +7,7 @@
 # Platform.setVirtual(True)
 
 if runtime.isStarted('i01.opencv'):
-    i01_opencv.removeFilter("FaceDetectDNN")
+    i01_opencv.removeFilters()
     i01_headTracking.enable()
 else:
     i01_headTracking.enable()   
