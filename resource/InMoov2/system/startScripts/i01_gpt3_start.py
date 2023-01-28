aded7d29514fef90780cb0713fe5bb1263d84ce0
Langevin Gael <hairygael@gmail.com>
2023-01-28 11:25:01 +0100

    add start/stop gpt3 scripts

---
+++ resource/InMoov2/system/startScripts/i01_gpt3_start.py
@@ -0,0 +1,16 @@
+#########################################
+# i01_gpt3_start.py
+# categories: InMoov2
+# more info @: http://myrobotlab.org/service/InMoov
+#########################################
+# uncomment for virtual hardware
+# Platform.setVirtual(True) 
+
+i01_gpt3 = runtime.start('i01.gpt3','Gpt3')
+i01.warn("gpt3 needs an api key from OpenAI.com!")
+i01_mouth = runtime.start('i01.mouth','LocalSpeech')
+i01_ear = runtime.start('i01.ear','WebkitSpeechRecognition')
+
+i01_gpt3.attach('i01.mouth')
+i01_ear.attach('i01.gpt3')
+i01_mouth.attach('i01.ear')
---
+++ resource/InMoov2/system/startScripts/i01_gpt3_stop.py
@@ -0,0 +1,10 @@
+#########################################
+# i01_gpt3_stop.py
+# categories: InMoov2
+# more info @: http://myrobotlab.org/service/InMoov
+#########################################
+# uncomment for virtual hardware
+# Platform.setVirtual(True) 
+
+# release service gpt3
+Runtime.releaseService("i01.gpt3")
