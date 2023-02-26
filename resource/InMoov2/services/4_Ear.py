f0b9edc8f713de9c10e0cfc5ea85abae505da8e5
Langevin Gael <hairygael@gmail.com>
2023-02-26 15:29:41 +0100

    add a current state

--- resource/InMoov2/services/4_Ear.py
+++ resource/InMoov2/services/4_Ear.py
@@ -26,7 +26,7 @@ def onRecognized(text):
     lastRecognized=text
     #if i01.RobotIsStarted:
     if runtime.isStarted('i01.fsm'):
-      if i01_fsm.getCurrentState(wake):  
+      if i01_fsm.getCurrentState()=="applyingConfig" or "systemCheck":  
         if runtime.isStarted('i01.chatBot'):
           if i01_fsm.getCurrentState()=="sleeping" and unicode(text,'utf-8')==lockPhrase:sleepModeWakeUp()
           if i01_fsm.getCurrentState()=="wake" and not unicode(text,'utf-8')==lockPhrase:humanDetected()
