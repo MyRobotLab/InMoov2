1f8b76a14764627f1c4c7336e68ce5fff99531aa
Langevin Gael <hairygael@gmail.com>
2023-02-26 15:16:45 +0100

    Setup for FSM

--- resource/InMoov2/services/4_Ear.py
+++ resource/InMoov2/services/4_Ear.py
@@ -1,35 +1,16 @@
 # ##############################################################################
 #                 EAR SERVICE FILE
-# ##############################################################################
-
-# ##############################################################################
-#               PERSONNAL PARAMETERS
 # ##############################################################################  
-  
-#read current service part config based on file name
-ThisServicePart=RuningFolder+'config/service_'+os.path.basename(inspect.stack()[0][1]).replace('.py','')
 
-CheckFileExist(ThisServicePart)
-ThisServicePartConfig = ConfigParser.ConfigParser()
-ThisServicePartConfig.read(ThisServicePart+'.config')
-
-EarEngine=ThisServicePartConfig.get('MAIN', 'EarEngine')
-setContinuous=ThisServicePartConfig.getboolean('MAIN', 'setContinuous')
-setAutoListen=ThisServicePartConfig.getboolean('MAIN', 'setAutoListen')
-ForceMicroOnIfSleeping=True
-lockPhrase="wake up"
-unlockInsult="forgive me"
-try:
-  ForceMicroOnIfSleeping=ThisServicePartConfig.getboolean('MAIN', 'ForceMicroOnIfSleeping')
-  lockPhrase=unicode(ThisServicePartConfig.get('MAIN', 'MagicCommandToWakeUp'),'utf-8')
-except:
-  pass
 
 # ##############################################################################
 # MRL SERVICE CALL
 # ##############################################################################
-i01_ear=runtime.create("i01.ear", EarEngine)
-#ear=i01_ear
+#i01_ear=runtime.create("i01.ear", "WebkitSpeechRecognition")
+if runtime.isStarted('i01.ear'):
+  ear=i01_ear
+  lockPhrase=ear.getConfig().wakeWord
+unlockInsult="forgive me"
 
 # ##############################################################################
 # MRL SERVICE TWEAKS
@@ -40,12 +21,12 @@ i01_ear=runtime.create("i01.ear", EarEngine)
 global lastRecognized
 lastRecognized=""
 def onRecognized(text):
-  #RobotneedUpdate : fix about first question do you want to update
   if text!="":
     global lastRecognized
     lastRecognized=text
-    if i01.RobotIsStarted:
-      if isChatBotActivated:
-        if i01.RobotIsSleeping and unicode(text,'utf-8')==lockPhrase:sleepModeWakeUp()
-      if not i01.RobotIsSleeping and not unicode(text,'utf-8')==lockPhrase:
-        humanDetected()
+    #if i01.RobotIsStarted:
+    if runtime.isStarted('i01.fsm'):
+      if i01_fsm.getCurrentState(wake):  
+        if runtime.isStarted('i01.chatBot'):
+          if i01_fsm.getCurrentState()=="sleeping" and unicode(text,'utf-8')==lockPhrase:sleepModeWakeUp()
+          if i01_fsm.getCurrentState()=="wake" and not unicode(text,'utf-8')==lockPhrase:humanDetected()
--- resource/InMoov2/services/H_OpenWeatherMap.py
+++ resource/InMoov2/services/H_OpenWeatherMap.py
@@ -3,29 +3,34 @@
 #                 OPENWEATHERMAP FILE
 # ##############################################################################
 
-
-#parse config
-ThisServicePart=RuningFolder+'config/service_'+os.path.basename(inspect.stack()[0][1]).replace('.py','')
-
-CheckFileExist(ThisServicePart)
-ThisServicePartConfig = ConfigParser.ConfigParser()
-ThisServicePartConfig.read(ThisServicePart+'.config')
-setUnits=ThisServicePartConfig.get('MAIN', 'setUnits')
-apikey=ThisServicePartConfig.get('MAIN', 'apikey')
-town=ThisServicePartConfig.get('MAIN', 'town').replace('"',"")
-
-openWeatherMap=runtime.start("openWeatherMap", "OpenWeatherMap")
-openWeatherMap.setKey(apikey)
-openWeatherMap.setUnits(setUnits)
+security.loadStore()
+apikey = security.getSecret("OPENWEATHERMAP")
 
 # forecast index 1 is next 3 hours , so 24 hours is 8
-def isTheSunShiny(townParam="town",period=1):  
-  if townParam=="town" or townParam=="":townParam=town
-  openWeatherMap.setLocation(townParam)
-  openWeatherMap.setPeriod(period)
-  curtemperature=openWeatherMap.getDegrees()
-  rawCode=openWeatherMap.getWeatherCode()
-  if not rawCode==0:
-    i01_chatBot.getResponse("SYSTEM METEO curtemperature " + str(int(curtemperature)) + " Town " + str(townParam.split(',')[0] + " COMMENTAIRE " + str(rawCode)))
+def isTheSunShiny(townParam="town",period=1):
+  if runtime.isStarted('i01.openWeatherMap'):
+    if apikey==None:
+      i01_chatBot.getResponse("SYSTEM openweathermapError")
+      runtime.error("open weathermap, key error")
+    else:
+      openWeatherMap=i01_openWeatherMap
+      town=openWeatherMap.getConfig().currentTown
+      if town==None:
+        i01_chatBot.getResponse("SYSTEM openweathermapError")
+        runtime.error("open weathermap error, this town is unrecognized")
+      else:
+        setUnits=openWeatherMap.getConfig().currentUnits
+        if setUnits==None:
+          i01_chatBot.getResponse("SYSTEM openweathermapError")
+          runtime.error("open weathermap error, units are not defined")
+        else:
+          if townParam=="town" or townParam=="":townParam=town  
+          openWeatherMap.setUnits(setUnits)
+          openWeatherMap.setLocation(townParam)
+          openWeatherMap.setPeriod(period)
+          curtemperature=openWeatherMap.getDegrees()
+          rawCode=openWeatherMap.getWeatherCode()
+          i01_chatBot.getResponse("SYSTEM METEO curtemperature " + str(int(curtemperature)) + " Town " + str(townParam.split(',')[0] + " COMMENTAIRE " + str(rawCode)))
   else:
-    i01_chatBot.getResponse("SYSTEM openweathermapError")
+      i01_chatBot.getResponse("SYSTEM openweathermapNotStarted")
+      runtime.error("open weathermap not started or maybe not configured")
