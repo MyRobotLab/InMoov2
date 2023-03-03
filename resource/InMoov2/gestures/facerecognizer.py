3003058416822d493ed6c586667cda097747fc85
Langevin Gael <hairygael@gmail.com>
2023-03-03 16:23:34 +0100

    fixed gestures

--- resource/InMoov2/gestures/facerecognizer.py
+++ resource/InMoov2/gestures/facerecognizer.py
@@ -1,8 +1,12 @@
 def facerecognizer(): 
-  #you need to train at least 2 FACES !   
-  i01.cameraOn()
-  fr=i01.vision.setActiveFilter("FaceRecognizer")
-  fr.setMode(OpenCVFilterFaceRecognizer.Mode.TRAIN)
-  fr.train()# it takes some time to train and be able to recognize face
-  fr.setMode(OpenCVFilterFaceRecognizer.Mode.RECOGNIZE)
-  i01.finishedGesture()
+  #you need to train at least 2 FACES !
+  if runtime.isStarted('i01.opencv'):
+    i01.cameraOn()
+    i01_opencv.addFilter("FaceRecognizer")
+    fr = i01_opencv.getFilter("FaceRecognizer")
+    fr.setMode(OpenCVFilterFaceRecognizer.Mode.TRAIN)
+    fr.train()# it takes some time to train and be able to recognize face
+    fr.setMode(OpenCVFilterFaceRecognizer.Mode.RECOGNIZE)
+    i01.finishedGesture()
+  else:
+    errorSpokenFunc('OPENCVNOWORKY')  
