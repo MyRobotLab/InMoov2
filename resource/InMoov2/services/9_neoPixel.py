2330b84bca246b89755512d052c7ed6497522df3
Langevin Gael <hairygael@gmail.com>
2023-03-03 17:03:24 +0100

    fixed indent

--- resource/InMoov2/services/9_neoPixel.py
+++ resource/InMoov2/services/9_neoPixel.py
@@ -8,7 +8,7 @@
 # ##############################################################################
 
 #neopixel = runtime.start("neopixel","Neopixel")
-  if runtime.isStarted('i01.neopixel'):
-    i01.speakBlocking(i01.localize("STARTINGNEOPIXEL"))
-    if boot_green:
-      i01.setNeopixelAnimation("Theater Chase", 0, 255, 50, 1)
+if runtime.isStarted('i01.neopixel'):
+  i01.speakBlocking(i01.localize("STARTINGNEOPIXEL"))
+  if boot_green:
+    i01.setNeopixelAnimation("Theater Chase", 0, 255, 50, 1)
