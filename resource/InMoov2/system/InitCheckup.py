b29d45bcdfd79dbd514983cb6a8d7c63d5582daf
Langevin Gael <hairygael@gmail.com>
2023-03-05 01:07:25 +0100

    Update InitCheckup.py

--- resource/InMoov2/system/InitCheckup.py
+++ resource/InMoov2/system/InitCheckup.py
@@ -28,7 +28,7 @@ execfile('resource/InMoov2/system/Errors.py'.encode('utf8'))
 #if IsMute==1:i01.setMute(True)
 
 ################################
-# INIT.3 - services call
+# INIT.2 - services call
 ################################
 #we load services python side from services folder
 #for filename in sorted(os.listdir('resource/InMoov2/services')):    
@@ -36,7 +36,7 @@ execfile('resource/InMoov2/system/Errors.py'.encode('utf8'))
     #execfile('resource/InMoov2/services/'+filename.encode('utf8'))
     #if DEBUG==1:print filename
 #runtime.startConfig('InMoov2_Full')
-#execfile('resource/InMoov2/services/1_AudioFile.py')
+execfile('resource/InMoov2/services/1_AudioFile.py')
 execfile('resource/InMoov2/services/3_ImageDisplay.py')
 execfile('resource/InMoov2/services/4_Ear.py')
 #5_Mouth.py
@@ -57,7 +57,7 @@ execfile('resource/InMoov2/services/H_OpenWeatherMap.py')
 execfile('resource/InMoov2/services/K_FiniteStateMachine.py')
 
 ################################ 
-# INIT.6- inmoov loading
+# INIT.3- inmoov loading
 ################################
     
 #we launch Inmoov life
@@ -79,7 +79,7 @@ execfile('resource/InMoov2/life/sleepMode.py')
 #execfile('data/InMoov2/InMoov_custom.py')
 
 ################################
-# INIT.8 - yes there is no 7 :) great, inmoov is alive
+# INIT.4
 ################################
 #if DEBUG==1:runtime.setLogLevel("INFO")
 #else:runtime.setLogLevel("ERROR")
@@ -95,86 +95,3 @@ if runtime.isStarted('i01.neopixel'):
 sleep(1)
 sleepModeWakeUp()
 
-# INIT.3 - services call
-################################
-#we load services python side from services folder
-#I have some strange no blocking event with LoadGesture so use classic execfile
-for filename in sorted(os.listdir(RuningFolder+'services')):    
-  if os.path.splitext(filename)[1] == ".py":
-    execfile(RuningFolder+'services/'+filename.encode('utf8'))
-    if DEBUG==1:print filename
-if LoadingPicture:display(RuningFolder+'/system/pictures/loading_1024-600.jpg')
-#mrl too old dude, update it !
-#if actualVersion<int(mrlCompatible):errorSpokenFunc('MRLNEEDUPDATE')   
- 
-################################
-# INIT.4 - skeleton loading & virtual skeleton
-################################
-#we launch Inmoov Skeleton at half speed
-i01.halfSpeed()
-
-for filename in os.listdir(RuningFolder+'skeleton'):    
-  if os.path.splitext(filename)[1] == ".py":execfile(RuningFolder+'skeleton/'+filename.encode('utf8'))
-
-
-if ((not isEyeLidsActivated and not isHeadActivated and not isLeftArmActivated and not isLeftHandActivated and not isRightArmActivated and not isRightHandActivated and not isTorsoActivated) and (ScriptType!="Virtual" and ScriptType!="NoArduino")):
-  i01_mouth.speakBlocking(i01.localize("NOSKELETON"))
-
-  
-################################
-# INIT.5 - ear.addcmmands
-################################
-#if chatbot loaded we do not load ear.addcommands
-for root, subdirs, files in os.walk(RuningFolder+'minimal'):
-  for name in files:
-    if name.split(".")[-1] == "py":
-      if (isChatBotActivated and name.split(".")[0][:11] == "earCommands"):
-        print name.split(".")[0]," not loaded because chatbot is activated"
-      else:  
-        execfile(os.path.join(root, name).encode('utf8'))
-        if DEBUG==1:print "debug  ear.addcmmands : ",os.path.join(root, name)    
-
-
-################################
-# INIT.6- inmoov loading
-################################
-  
-#we launch Inmoov Gestures
-i01.loadGestures(RuningFolder+'gestures')
-    
-#we launch Inmoov life
-for root, subdirs, files in os.walk(RuningFolder+'life'):
-  for name in sorted(files):
-    if name.split(".")[-1] == "py":
-      execfile(os.path.join(root, name))
-      if DEBUG==1:print "debug inmoovLife : ",os.path.join(root, name)
-
-#create the custom script, only if not exist
-if not os.path.isfile(RuningFolder+'custom/InMoov_custom.py'):shutil.move(RuningFolder+'custom/InMoov_custom.py.default',RuningFolder+'custom/InMoov_custom.py')
-  #clean up .default.config
-  
-#remove default config files
-for root, subdirs, files in os.walk(RuningFolder):
-  for name in files:
-    if name.split(".")[-1] == "default":
-      os.remove(os.path.join(root, name))
-      if DEBUG==1:print "removed .default : ",os.path.join(root, name)
-
-# launch custom script
-execfile(RuningFolder+'/custom/InMoov_custom.py')
-      
-################################
-# INIT.8 - yes there is no 7 :) great, inmoov is alive
-################################
-if DEBUG==1:runtime.setLogLevel("INFO")
-else:runtime.setLogLevel("ERROR")
-i01.setMute(False)
-
-if boot_green and runtime.isStarted('i01.neopixel'):    
-  i01.setNeopixelAnimation("Flash Random", 0, 255, 0, 1)
-  sleep(2)
-  i01.stopNeopixelAnimation()
-  sleep(1)
-  i01.setNeopixelAnimation("Flash Random", 0, 255, 50, 10)
-sleep(1)
-sleepModeWakeUp()
