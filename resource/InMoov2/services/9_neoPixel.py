76ee311af70787e4f203c4ab775573d33420812b
Langevin Gael <hairygael@gmail.com>
2023-03-16 19:01:38 +0100

    Update 9_neoPixel.py

--- resource/InMoov2/services/9_neoPixel.py
+++ resource/InMoov2/services/9_neoPixel.py
@@ -7,8 +7,8 @@
 #                 SET SERVICE
 # ##############################################################################
 
-#i01_neoPixel = runtime.start("i01.neoPixel","Neopixel")
 if runtime.isStarted('i01.neoPixel'):
-  i01.speakBlocking(i01.localize("STARTINGNEOPIXEL"))
   if boot_green==1:
     i01_neoPixel.setAnimation("Theater Chase", 0, 255, 50, 10)
+  if runtime.isStarted('i01'):
+    i01.speakBlocking(i01.localize("STARTINGNEOPIXEL"))
