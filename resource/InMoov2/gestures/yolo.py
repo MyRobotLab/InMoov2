dc04455f41078ee014df9ee6f53d12a4c4f52c4a
Langevin Gael <hairygael@gmail.com>
2023-03-29 22:28:33 +0200

    fixed yolo.py

--- resource/InMoov2/gestures/yolo.py
+++ resource/InMoov2/gestures/yolo.py
@@ -2,13 +2,19 @@
 
 # We classify objects per frame and get the maximum detected for 1 frame only
 # So we can list & count at one time, every available classified object in the field of view, in given time
-# Also get given element (label) position if set, from all others in the field off view, from left to right
+# Also get given element (last_item_found) position if set, from all others in the field off view, from left to right
 
-# "i01_vision_collectionCount" is the dictionary for detected objects in given time
+# "classifications" is the dictionary for detected objects in given time
 # after aquisition & position sort, we will read collection content to play with it
 
-## warning : yolo publisher is now inside java land to avoid threading issues because of python sleep
-
+def onClassification(data):
+    global last_item_found
+    last_item_found = None
+    global classifications, last_item_found
+    classifications = data
+    for key, value in data.items():
+        #print(key)
+        last_item_found = key
 
 def startYoloInventory(duration):
   i01.speak(i01_chatBot.getPredicate("startupSentence"))
@@ -18,44 +24,42 @@ def startYoloInventory(duration):
   enableYoloFor(duration)
   # interpret results ...
   collectionString=""
-  for key, value in i01_vision_collectionCount.iteritems():
-    collectionString=collectionString+str(value)+" "+key+", "
+  for key, value in classifications.items():
+    #collectionString=collectionString+str(value)+" "+key+", "
+    collectionString=collectionString+" "+key+", "
+    print('object detected: ',collectionString)
   #check if we have results, return key "none" if no ( aiml will understand the key 'none' )
   if len(collectionString) == 0:collectionString="none"
   # return results
-  i01_chatBot.setPredicate("yoloTotalDetected",str(len(i01_vision_collectionCount)))
+  i01_chatBot.setPredicate("yoloTotalDetected",str(len(classifications)))
   return collectionString
 
-def getYoloPosition(label):
-  position=i01_vision.getPosition(label)
+def getYoloPosition(last_item_found):
+  position = classifications.getPosition(last_item_found)
   # to not launch gesture, comment showObject :
-  print "Position of : ",label,position
+  print "Position of : ", last_item_found, position
   showObject(position)
-  return position  
-
-# TODO classify while head moves ! ( count )
-# TODO what do you see in front of you ( few centimeters -> use gael mods )
+  return position
 
 global lastPhotoFileName
 #shared function to start & stop yolo filter
 def enableYoloFor(duration):
   global lastPhotoFileName
-  i01_vision_collectionCount.clear()
-  i01_vision_collectionPositions.clear()
   #start i01.opencv
-  i01.cameraOn()
-  # yolo filter in the pipeline ( add if no exist + enable + setActive )
-  i01_vision.setActiveFilter("Yolo")
-  try:
-    gui.setActiveTab("i01_opencv")
-    #os.remove(lastPhotoFileName)
-    imageDisplay.closeAll()
-  except:
-    pass
-  # wait for X
-  sleep(duration)
-  lastPhotoFileName = i01_opencv.recordFrame()
-  #print lastPhotoFileName
-  imageDisplay.display(lastPhotoFileName)
-  i01_opencv.disableFilter("Yolo")
-  print i01_vision_collectionCount
+  if runtime.isStarted('i01.opencv'):
+    i01_opencv.addFilter("yolo")
+    i01_opencv.setDisplayFilter("yolo")
+    i01_opencv.setCameraIndex(0)
+    i01_opencv.capture()
+    classifications = {}
+    # create a subscription to classification publication
+    python.subscribe('i01.opencv', 'publishClassification')
+    # wait for X
+    sleep(duration)
+    lastPhotoFileName = i01_opencv.recordFrame()
+    classifications.clear()
+    python.unsubscribe('i01.opencv', 'publishClassification')
+    i01_opencv.disableFilter("yolo")
+    i01_opencv.removeFilter('yolo')
+    i01_opencv.stopCapture()
+    i01.cameraOff()
