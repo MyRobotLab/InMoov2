3fafd902e627257fa27d5a536a50e18edcb276e2
Langevin Gael <hairygael@gmail.com>
2023-04-17 12:13:51 +0200

    update gpt3

--- resource/InMoov2/system/startScripts/i01_gpt3_start.py
+++ resource/InMoov2/system/startScripts/i01_gpt3_start.py
@@ -8,19 +8,17 @@
 
 i01.startPeer('gpt3')
 i01.warn("gpt3 needs an api key from OpenAI.com!")
-
-if runtime.isStarted('i01.chatBot'):
-    ## we need to run the session
-    #i01_chatBot.getResponse("SYSTEM_EVENT STARTED GPT3")
-    i01_chatBot.setPredicate("gpt3", "enabled")
-    i01_chatBot.savePredicates()
-    if runtime.isStarted('i01.mouth'):
-        i01_gpt3.attach('i01.mouth')
+if runtime.isStarted('i01.gpt3'):
+    if runtime.isStarted('i01.chatBot'):
+        i01_chatBot.setPredicate("gpt3", "enabled")
+        i01_chatBot.savePredicates()
+        if runtime.isStarted('i01.mouth'):
+            i01_gpt3.attach('i01.mouth')
+        else:
+            i01.startPeer('mouth')
+            i01_gpt3.attach('i01.mouth')  
     else:
         i01.startPeer('mouth')
-        i01_gpt3.attach('i01.mouth')  
-else:
-    i01.startPeer('mouth')
-    i01.startPeer('ear')
-    i01_gpt3.attach('i01.mouth')
-    i01_ear.attach('i01.gpt3')
+        i01.startPeer('ear')
+        i01_gpt3.attach('i01.mouth')
+        i01_ear.attach('i01.gpt3')
