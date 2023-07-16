a15b3f234d3397c4db7272107e040b348ae7e527
Langevin Gael <hairygael@gmail.com>
2023-07-16 12:34:00 +0200

    Update trackPoint.py

--- gestures/trackPoint.py
+++ gestures/trackPoint.py
@@ -1,10 +1,14 @@
 def trackPoint():
-  i01.cameraOn()
-  i01.trackPoint()
-  try:gui.setActiveTab("i01.opencv")
-  except:pass
-  i01_head_rollNeck.setAutoDisable(False)
-  i01_head_rollNeck.moveToBlocking(90)
-  i01.setHeadSpeed(100.0,100.0,100.0,100.0,100.0)
-  #i01.headPid.setPID("x",12,5,0.1)
-  #i01.headPid.setPID("y",12,5,0.1)
+  if runtime.isStarted('i01.opencv'):
+    i01_head_rollNeck.setAutoDisable(False)
+    i01_head_rollNeck.moveToBlocking(90)
+    i01.setHeadSpeed(500.0,500.0,500.0,500.0,500.0)
+    i01_opencv.removeFilters()
+    try:gui.setActiveTab("i01.opencv")
+    except:pass
+    i01_opencv.addFilter("LKOpticalTrack")
+    tr = i01_opencv.getFilter("LKOpticalTrack")
+    tr.addPoint(280, 280)
+    i01_opencv.capture()
+  else:
+    i01.warn("i01.opencv needs to be started!"))
