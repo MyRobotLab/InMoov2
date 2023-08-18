00028e675100df51666725640771ffc36d59a923
Langevin Gael <hairygael@gmail.com>
2023-08-18 14:12:52 +0200

    fix delay trackHumans.py

--- gestures/trackHumans.py
+++ gestures/trackHumans.py
@@ -4,9 +4,10 @@ def trackHumans():
     i01_head_rollNeck.moveToBlocking(90)
     i01.setHeadSpeed(500.0,500.0,500.0,500.0,500.0)
     i01_opencv.removeFilters()
-    #i01.getHeadTracking()
     i01.startPeer("headTracking")
-    i01_headTracking.enable()
+    sleep(2)
+    if runtime.isStarted('i01.headTracking'):
+      i01_headTracking.enable()
   else:
     i01.warn("i01.opencv needs to be started to start tracking!")
 
