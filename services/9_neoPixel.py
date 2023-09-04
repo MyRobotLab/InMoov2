e5b74aa36300c448c3407b6b9db8ac2b6955633c
Langevin Gael <hairygael@gmail.com>
2023-09-04 22:39:58 +0200

    moved error_red

--- services/9_neoPixel.py
+++ services/9_neoPixel.py
@@ -10,3 +10,9 @@
 if runtime.isStarted('i01.neoPixel'):
   if boot_green==1:
     i01_neoPixel.setAnimation("Theater Chase", 0, 255, 50, 10)
+def errors():
+  if error_red==1:
+      if runtime.isStarted('i01.neoPixel'):
+        i01_neoPixel.setAnimation("Theater Chase", 255, 0, 0, 50)
+        sleep(4)
+        i01_neoPixel.clear()    
