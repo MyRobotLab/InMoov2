c7de1e22e25380bb033b0b6590684b60b3c44a15
Langevin Gael <hairygael@gmail.com>
2023-10-17 20:20:58 +0200

    Update 9_neoPixel.py

--- services/9_neoPixel.py
+++ services/9_neoPixel.py
@@ -8,11 +8,11 @@
 # ##############################################################################
 
 if runtime.isStarted('i01.neoPixel'):
-  if boot_green==1:
+  if i01.getConfig().neoPixelBootGreen==1:
     i01_neoPixel.setAnimation("Theater Chase", 0, 255, 50, 10)
 def errors():
-  if error_red==1:
+  if i01.getConfig().neoPixelErrorRed==1:
       if runtime.isStarted('i01.neoPixel'):
         i01_neoPixel.setAnimation("Theater Chase", 255, 0, 0, 50)
         sleep(4)
-        i01_neoPixel.clear()    
+        i01_neoPixel.clear()
