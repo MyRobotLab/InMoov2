8900cd0c9a6e1bd26aebbae164ecbd81e8fc08a0
Langevin Gael <hairygael@gmail.com>
2023-10-30 12:15:15 +0100

    Update K_FiniteStateMachine.py

--- services/K_FiniteStateMachine.py
+++ services/K_FiniteStateMachine.py
@@ -3,12 +3,15 @@
 #                 FINITE STATE MACHINE SERVICE FILE
 # ##############################################################################
 
-i01.startPeer('fsm')
-if runtime.isStarted('i01.fsm'):
+i01_fsm = i01.startPeer('fsm')
+def initFsm():
     i01_fsm.getConfig()
-
+    
+if runtime.isStarted('i01.fsm'):
+    initFsm()
     i01_fsm.addTransition("awake","track","isTracking")
     i01_fsm.addTransition("isTracking","stopTrack","awake")
     #i01_fsm.fire("clear-event")
     print(i01_fsm.getCurrent())
     sleep(1)
+    
