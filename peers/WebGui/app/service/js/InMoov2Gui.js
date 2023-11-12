2b1d092f0b1c78b4a8f6fa8b9cc31db6046433af
greg perry <greg.perry@torc.ai>
2023-11-11 19:05:48 -0800

    fix for speech type

--- peers/WebGui/app/service/js/InMoov2Gui.js
+++ peers/WebGui/app/service/js/InMoov2Gui.js
@@ -29,6 +29,8 @@ angular.module('mrlapp.service.InMoov2Gui', []).controller('InMoov2GuiCtrl', ['$
     $scope.languageSelected = null
     $scope.speakText = null
     $scope.toggleValue = true
+    
+    $scope.speechType = 'MarySpeech'
 
     $scope.activePanel = 'settings'
 
