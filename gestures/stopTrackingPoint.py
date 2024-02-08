0c7541d1c5af1cac8232175f9751754038d47ad6
Langevin Gael <hairygael@gmail.com>
2024-02-08 17:25:29 +0100

    add if runtime.isStarted

--- gestures/stopTrackingPoint.py
+++ gestures/stopTrackingPoint.py
@@ -2,6 +2,8 @@ def stopTrackingPoint():
   if runtime.isStarted("i01.opencv"):
     if runtime.isStarted('i01.fsm'):
       i01_fsm.fire("stopTrack")
-    i01_opencv.removeFilter("LKOpticalTrack")
-    i01_head_rollNeck.setAutoDisable(True)
+    if runtime.isStarted("i01.head"):
+      i01_head_rollNeck.setAutoDisable(True)
+    i01_opencv.removeFilters()
     i01_opencv.stopCapture()
+    i01.finishedGesture()
