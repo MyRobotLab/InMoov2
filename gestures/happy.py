55e5171325d3b81e934c187e505e45e59a9e115e
Langevin Gael <hairygael@gmail.com>
2024-02-08 12:17:52 +0100

    Update happy.py

--- gestures/happy.py
+++ gestures/happy.py
@@ -1,30 +1,20 @@
 def happy():
   #i01.startedGesture()
-  for w in range(0,3):
-    sleep(1)
-    i01.setHandSpeed("left", 100.0, 100.0, 100.0, 100.0, 100.0, 100.0)
-    i01.setHandSpeed("right", 100.0, 100.0, 100.0, 100.0, 100.0, 100.0)
-    i01.setArmSpeed("right", 43.0, 43.0, 43.0, 100.0)
-    i01.setArmSpeed("left", 100.0, 100.0, 100.0, 100.0)
+  if runtime.isStarted('i01.head'):
     i01.setHeadSpeed(22.0, 22.0)
     i01.moveHead(84,88)
+  if runtime.isStarted('i01.leftArm'):  
     i01.moveArm("left",5,82,36,10)
+    i01.setArmSpeed("left", 100.0, 100.0, 100.0, 100.0)
+  if runtime.isStarted('i01.rightArm'):  
+    i01.setArmSpeed("right", 43.0, 43.0, 43.0, 100.0)
     i01.moveArm("right",74,112,61,29)
+  if runtime.isStarted('i01.leftHand'):
+    i01.setHandSpeed("left", 100.0, 100.0, 100.0, 100.0, 100.0, 100.0)
     i01.moveHand("left",0,88,135,94,96,90)
+  if runtime.isStarted('i01.rightHand'):  
+    i01.setHandSpeed("right", 100.0, 100.0, 100.0, 100.0, 100.0, 100.0)
     i01.moveHand("right",81,79,118,47,0,90)
-    sleep(1)
-    if w==1:
-        i01.speakBlocking("happy birthday grog")
-        i01.setHandSpeed("left", 100.0, 100.0, 100.0, 100.0, 100.0, 100.0)
-        i01.setHandSpeed("right", 100.0, 100.0, 100.0, 100.0, 100.0, 100.0)
-        i01.setArmSpeed("right", 43.0, 43.0, 43.0, 100.0)
-        i01.setArmSpeed("left", 100.0, 100.0, 100.0, 100.0)
-        i01.setHeadSpeed(22.0, 22.0)
-        i01.moveHead(42,76)
-        i01.moveArm("left",5,90,30,10)
-        i01.moveArm("right",74,70,61,10)
-        i01.moveHand("left",0,0,0,0,0,90)
-        i01.moveHand("right",81,79,118,47,0,90)
-        sleep(5)
-  i01.startedGesture()    
-        
+  sleep(5)
+  i01.finishedGesture()
+  rest()
