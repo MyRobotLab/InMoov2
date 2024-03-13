6ff27f4fc8f7350f8c7b98d57a8ecd584e78d8e7
grog <grog@myrobotlab.org>
2024-03-12 22:07:47 -0700

    removed K_FiniteStateMachine.py

--- init/InitCheckUp.py
+++ init/InitCheckUp.py
@@ -5,67 +5,67 @@
 ################################
 # INIT.1 - system dependencies
 ################################
-DEBUG=0
+DEBUG = 0
 # libraries import
-execfile('resource/InMoov2/system/Import_Libraries.py')
+execfile("resource/InMoov2/system/Import_Libraries.py")
 # common functions
-execfile('resource/InMoov2/system/Import_Functions.py')
+execfile("resource/InMoov2/system/Import_Functions.py")
 
-#RuningFolder=os.getcwd().replace("\\", "/")+"/"+RuningFolder+"/"
+# RuningFolder=os.getcwd().replace("\\", "/")+"/"+RuningFolder+"/"
 # global vars import
-execfile('resource/InMoov2/system/Robot_Satus_GlobalsVars.py')
+execfile("resource/InMoov2/system/Robot_Satus_GlobalsVars.py")
 
 # we load our custom script
-execfile('resource/InMoov2/custom/InMoovCustom_start.py')
+execfile("resource/InMoov2/custom/InMoovCustom_start.py")
 
 # we load alan's face for facerecognition
-execfile('resource/InMoov2/system/startScripts/InMoovTrain_start.py')
+execfile("resource/InMoov2/system/startScripts/InMoovTrain_start.py")
 
 # we get InMoov2 service
 i01 = Runtime.getService("i01")
 
 # mute for starting action vocals
-#if IsMute==1:i01.setMute(True)
+# if IsMute==1:i01.setMute(True)
 
 ################################
 # INIT.2 - services call
 ################################
-#we load services python side from services folder
-execfile('resource/InMoov2/services/1_AudioFile.py')
-execfile('resource/InMoov2/services/3_ImageDisplay.py')
-execfile('resource/InMoov2/services/4_Ear.py')
-execfile('resource/InMoov2/services/5_Mouth.py')
-#8_NervoBoardRelay.py  ##NEED FIXING FOR NIXIE
-execfile('resource/InMoov2/services/9_neoPixel.py')
-execfile('resource/InMoov2/services/A_Chatbot.py')
-execfile('resource/InMoov2/services/C_Pir.py')
-execfile('resource/InMoov2/services/D_OpenCv.py')
-execfile('resource/InMoov2/services/E_OpenNi.py')
-#E_Oak.py ## WOULD REPLACE KINECT
-#G_Translator.py ## NEED FIXING FOR NIXIE
-execfile('resource/InMoov2/services/H_OpenWeatherMap.py')
-#J_SensorFinger.py ## NEED FIXING FOR NIXIE
-execfile('resource/InMoov2/services/K_FiniteStateMachine.py')
+# we load services python side from services folder
+execfile("resource/InMoov2/services/1_AudioFile.py")
+execfile("resource/InMoov2/services/3_ImageDisplay.py")
+execfile("resource/InMoov2/services/4_Ear.py")
+execfile("resource/InMoov2/services/5_Mouth.py")
+# 8_NervoBoardRelay.py  ##NEED FIXING FOR NIXIE
+execfile("resource/InMoov2/services/9_neoPixel.py")
+execfile("resource/InMoov2/services/A_Chatbot.py")
+execfile("resource/InMoov2/services/C_Pir.py")
+execfile("resource/InMoov2/services/D_OpenCv.py")
+execfile("resource/InMoov2/services/E_OpenNi.py")
+# E_Oak.py ## WOULD REPLACE KINECT
+# G_Translator.py ## NEED FIXING FOR NIXIE
+execfile("resource/InMoov2/services/H_OpenWeatherMap.py")
+# J_SensorFinger.py ## NEED FIXING FOR NIXIE
+# execfile('resource/InMoov2/services/K_FiniteStateMachine.py')
 
-################################ 
+################################
 # INIT.3- inmoov loading
 ################################
-    
-#we launch Inmoov life
-execfile('resource/InMoov2/life/HealthCheck.py')
-execfile('resource/InMoov2/life/shutdown.py')
-execfile('resource/InMoov2/life/shutdownComplete.py')
-execfile('resource/InMoov2/life/sleepMode.py')
+
+# we launch Inmoov life
+execfile("resource/InMoov2/life/HealthCheck.py")
+execfile("resource/InMoov2/life/shutdown.py")
+execfile("resource/InMoov2/life/shutdownComplete.py")
+execfile("resource/InMoov2/life/sleepMode.py")
 
 ################################
 # INIT.4 - great, inmoov is alive
 ################################
 
-if runtime.isStarted('i01.neopixel'):    
-  i01.setNeopixelAnimation("Flash Random", 0, 255, 0, 1)
-  sleep(2)
-  i01.stopNeopixelAnimation()
-  sleep(1)
-  i01.setNeopixelAnimation("Flash Random", 0, 255, 50, 10)
+if runtime.isStarted("i01.neopixel"):
+    i01.setNeopixelAnimation("Flash Random", 0, 255, 0, 1)
+    sleep(2)
+    i01.stopNeopixelAnimation()
+    sleep(1)
+    i01.setNeopixelAnimation("Flash Random", 0, 255, 50, 10)
 sleep(1)
 welcomeMessage()
