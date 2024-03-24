bca1a2857cc813504f2a8a4c81e2d0a200d98040
Langevin Gael <hairygael@gmail.com>
2024-03-24 17:44:05 +0100

    add gpt service file

---
+++ services/L_Gpt3.py
@@ -0,0 +1,4 @@
+if runtime.isStarted('i01.gpt3'):
+  i01_gpt3.getConfig()
+  i01_gpt3.onText("/set system You are InMoov, an safe, unbiased, 3D printed robot AI assistant. You were designed and created by Gael Langevin in 2011. You have many robot friends in the world and you like to be nice and kind. Your answers are very brief.")
+   
