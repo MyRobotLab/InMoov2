228d0673b97b76709eca1e8f7c4f375ba03ea483
Langevin Gael <hairygael@gmail.com>
2024-04-01 12:53:01 +0200

    another update

--- services/5_Mouth.py
+++ services/5_Mouth.py
@@ -16,25 +16,13 @@ def onEndSpeaking(text):
   if runtime.isStarted('i01'):
     if i01.getConfig().robotCanMoveHeadWhileSpeaking==1:
       if runtime.isStarted('i01.random'):
-        i01_random.disable('randomFace')
-        if runtime.isStarted('i01.leftArm'):
-          i01_random.enable('i01.setLeftArmSpeed')
-          i01_random.enable('i01.moveLeftArm')
-        if runtime.isStarted('i01.rightArm'):
-          i01_random.enable('i01.setRightArmSpeed')
-          i01_random.enable('i01.moveRightArm')
-        if runtime.isStarted('i01.leftHand'):
-          i01_random.enable('i01.setLeftHandSpeed')
-          i01_random.enable('i01.moveLeftHand')
-        if runtime.isStarted('i01.rightHand'):
-          i01_random.enable('i01.setRightHandSpeed')
-          i01_random.enable('i01.moveRightHand')
-        if runtime.isStarted('i01.torso'):
-          i01_random.enable('i01.setTorsoSpeed')
-          i01_random.enable('i01.moveTorso')
         if runtime.isStarted('i01.head'):
           i01_random.addRandom(200, 1000, "i01", "setHeadSpeed", 8.0, 20.0, 8.0, 20.0, 8.0, 20.0)
           i01_random.addRandom(200, 1000, "i01", "moveHead", 70.0, 110.0, 65.0, 115.0, 70.0, 110.0)
+          i01_random.disable('i01.setHeadSpeed')
+          i01_random.disable('i01.moveHead')
+          i01_random.disable('randomBlink')
+          i01_random.disable('randomFace')
     if i01.getConfig().robotCanMoveHeadWhileSpeaking==0:
       if runtime.isStarted('i01.random'):
         if runtime.isStarted('i01.head'):
@@ -53,8 +41,8 @@ def onStartSpeaking(text):
 
     #force random move while speaking, to avoid conflict with random life gesture
     if i01.getConfig().robotCanMoveHeadWhileSpeaking==1:
-      i01_random = i01.startPeer('random')
-      if runtime.isStarted('i01.random'):
+      if not runtime.isStarted('i01.random'):
+        i01_random = i01.startPeer('random')
         if runtime.isStarted('i01.leftArm'):
           i01_random.disable('i01.setLeftArmSpeed')
           i01_random.disable('i01.moveLeftArm')
@@ -70,7 +58,7 @@ def onStartSpeaking(text):
         if runtime.isStarted('i01.torso'):
           i01_random.disable('i01.setTorsoSpeed')
           i01_random.disable('i01.moveTorso')
-        i01_random.enable()  
+        i01_random.enable()
         if runtime.isStarted('i01.head'):
           if runtime.isStarted('i01.opencv'):
             if runtime.isStarted('i01.fsm'):
@@ -89,7 +77,27 @@ def onStartSpeaking(text):
               i01_random.enable('randomBlink')
               i01_random.addRandom("randomFace", 1000, 2000, "python", "exec", "blink()", "upperLipU()", "upperLipD()", "cheeksU()", "cheeksD()", "forheadsU()", "forheadsD()")
               i01_random.enable('randomFace')
+      else:
+        i01_random = i01.startPeer('random')
+        if runtime.isStarted('i01.random'):
+          i01_random.enable()
+          if runtime.isStarted('i01.head'):
+            if runtime.isStarted('i01.opencv'):
+              if runtime.isStarted('i01.fsm'):
+                if not i01_fsm.getState()=="isTracking":
+                    i01_random.addRandom(200, 1000, "i01", "setHeadSpeed", 8.0, 20.0, 8.0, 20.0, 8.0, 20.0)
+                    i01_random.addRandom(100, 500, "i01", "moveHead", 70.0, 110.0, 65.0, 115.0, 70.0, 110.0)
+                    i01_random.enable('i01.setHeadSpeed')
+                    i01_random.enable('i01.moveHead')
+                    i01_random.addRandom("randomBlink", 5000, 30000, "python", "exec", "blink()")
+                    i01_random.enable('randomBlink')
+                    i01_random.addRandom("randomFace", 1000, 2000, "python", "exec", "blink()", "upperLipU()", "upperLipD()", "cheeksU()", "cheeksD()", "forheadsU()", "forheadsD()")
+                    i01_random.enable('randomFace')
 
+            else:
+                i01_random.addRandom("randomBlink", 5000, 30000, "python", "exec", "blink()")
+                i01_random.enable('randomBlink')
+                i01_random.addRandom("randomFace", 1000, 2000, "python", "exec", "blink()", "upperLipU()", "upperLipD()", "cheeksU()", "cheeksD()", "forheadsU()", "forheadsD()")
 
 def onFilterText(text):
   # filter all you want here
@@ -117,10 +125,10 @@ def onFilterText(text):
   function_to_call = globals().get(extracted_word)
   if callable(function_to_call):
       result = function_to_call()
-      result
+      result   
   else:
       result = "No function found matching the extracted word."
       result
-    
+
 if runtime.isStarted('i01.mouth'):
   initMouth()
