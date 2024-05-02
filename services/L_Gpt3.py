6e82ac5ab780db52a855e82c3846e118b9a966ba
Langevin Gael <hairygael@gmail.com>
2024-05-02 12:58:44 +0200

    force face expressions

--- services/L_Gpt3.py
+++ services/L_Gpt3.py
@@ -2,4 +2,4 @@ if runtime.isStarted('i01.gpt3'):
   i01_gpt3.getConfig()
   setSystem = i01_gpt3.getConfig().prefix
   if setSystem == None:
-    i01_gpt3.onText("/set system You are InMoov, a safe, 3D printed robot with a physical body. You were designed in 2011. Your answers are super brief.")
+    i01_gpt3.onText("/set system You are InMoov, a safe, 3D printed robot with a physical body. You were designed in 2011. Your answers are super brief. Produce face expressions in single word between asterisks.")
