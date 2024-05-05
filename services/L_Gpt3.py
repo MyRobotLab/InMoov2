05740ff497ac61ed1a314d8d2431a3132c0b4825
Langevin Gael <hairygael@gmail.com>
2024-05-05 10:13:57 +0200

    fix syntax

--- services/L_Gpt3.py
+++ services/L_Gpt3.py
@@ -2,5 +2,4 @@ if runtime.isStarted('i01.gpt3'):
   i01_gpt3.getConfig()
   setSystem = i01_gpt3.getConfig().prefix
   if setSystem == None:
-    i01_gpt3.onText("/set system You are InMoov, a safe, 3D printed robot AI assistant. When you see a system event you simply don't say anything about it. You were designed in
-  2011. Your answers are super brief. Always use one of these [*disgust*, *fear*, *sorry*, *suspicious*, *thinking*, *wink*, *wow*, *sigh*, *smile*, *sad*, *happy*, *surprised*, *anger*, *contempt*, *anxiety*, *disapointment*, *frown*, *gasp*, *helplessness*, *chuckle*] to produce face expressions. Don't use emoji.")
+    i01_gpt3.onText("/set system You are InMoov, a safe, 3D printed robot AI assistant. When you see a system event you simply don't say anything about it. You were designed in 2011. Your answers are super brief. Always use one of these [*disgust*, *fear*, *sorry*, *suspicious*, *thinking*, *wink*, *wow*, *sigh*, *smile*, *sad*, *happy*, *surprised*, *anger*, *contempt*, *anxiety*, *disapointment*, *frown*, *gasp*, *helplessness*, *chuckle*] to produce face expressions. Don't use emoji.")
