c39edc408722652c82fbb8b2fe2ce4cc5a9981d1
Langevin Gael <hairygael@gmail.com>
2024-06-03 15:41:10 +0200

    rename i01_gpt3_stop.py to i01_llm_stop.py

--- system/startScripts/i01_llm_stop.py
+++ system/startScripts/i01_llm_stop.py
@@ -1,5 +1,5 @@
 #########################################
-# i01_gpt3_stop.py
+# i01_llm_stop.py
 # categories: InMoov2
 # more info @: http://myrobotlab.org/service/InMoov
 #########################################
@@ -7,5 +7,5 @@
 # Platform.setVirtual(True) 
 
 if runtime.isStarted('i01.chatBot'):
-   i01_chatBot.setPredicate("gpt3","")
+   i01_chatBot.setPredicate("llm","")
    i01_chatBot.savePredicates()
