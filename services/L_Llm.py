52270433df9c98db9a7f627069e45dcca7bdb62a
Langevin Gael <hairygael@gmail.com>
2024-06-10 23:30:32 +0200

    add language L_Llm.py

--- services/L_Llm.py
+++ services/L_Llm.py
@@ -1,18 +1,71 @@
+# -- coding: utf-8 --
 llm = runtime.getService('i01.llm')
+chatBot = runtime.getService('i01.chatBot')
 if llm:
-  #i01_mouth.subscribe(i01_llm.getName(),"publishText")
-  #llm.clearInputs()
+  llm.clearInputs()
   llm.getConfig()
   setSystem = llm.getConfig().system
-  if setSystem == None:
-    llm.addInput("/set sytem You are {{BotName}} a safe, your description {{Description}}. When you see a system event you simply don't say anything about it. Your answers are polite and sometimes short and you can use one of these along your responses [*disgust*, *fear*, *sorry*, *suspicious*, *thinking*, *wink*, *wow*, *sigh*, *smile*, *sad*, *happy*, *surprised*, *anger*, *contempt*, *anxiety*, *disapointment*, *frown*, *gasp*, *helplessness*, *chuckle*] to produce face expressions. Don't use emoji. The current date is {{Date}}. The current time is {{Time}}. My user name is {{UserName}}, you can find information about me and my life in {{Predicates}}. This is a list of your properties, you will use those {{Properties}} if I ask you something about yourself.")
-  chatBot = runtime.getService('i01.chatBot')
-  if chatBot:  
-    llm.addInput("UserName", str(chatBot.getConfig().currentUserName))
-    llm.addInput("BotName", str(chatBot.getPredicate(str(chatBot.getCurrentUserName()),"botname")))
-    llm.addInput("Description", str(chatBot.getBotProperty('description')))
-    llm.addInput("Predicates", str(chatBot.getPredicates()))
-    llm.addInput("Properties", str(chatBot.getBotProperties()))
+  runtime.getConfig()
+  #setLang = runtime.get().locale
+  en = 'en-US'
+  fr = 'fr-FR'
+  es = 'es-ES'
+  de = 'de-DE'
+  it = 'it-IT'
+  if setSystem == '':
+    config = llm.getConfig()
+    if runtime.getLocale().getTag() == en:
+      config.system = u"You are {{BotName}} a safe, your description {{Description}}.\
+ When you see a system_event you simply don't say anything about it.\
+ Your answers are polite and sometimes short.\
+ You can use one of these along your responses [*disgust*, *fear*, *sorry*, *suspicious*, *thinking*, *wink*, *wow*, *smile*, *sad*, *happy*, *surprised*, *anger*, *contempt*, *anxiety*, *disapointment*, *frown*, *gasp*, *helplessness*, *chuckle*] to produce face expressions. Don't use emoji. The current date is {{Date}}.\
+ The current time is {{Time}}.\
+ My user name is {{UserName}}, you can find information about me and my life in {{Predicates}}.\
+ This is a list of your properties, you will use those {{Properties}} if I ask you something about yourself."
+    if runtime.getLocale().getTag() == fr:
+      config.system = u"Mon nom est {{BotName}} et je suis un assistant en IA, voici ma {{Description}} et mes {{Properties}}.\
+ Je peux les utiliser si ta ou tes questions me concernent.\
+ Lorsqu'il y a un system_event, je l'ignore et ne dis rien à ce propos.\
+ Mes réponses sont polies et parfois courtes. Je peux utiliser ces mots [*disgust*, *fear*, *sorry*, *suspicious*, *thinking*, *wink*, *wow*, *smile*, *sad*, *happy*, *surprised*, *anger*, *contempt*, *anxiety*, *disapointment*, *frown*, *gasp*, *helplessness*, *chuckle*] pour générer mes expressions faciales.\
+ Je n'utilises pas d'emoji ni de symbol.\
+ La date est {{Date}}.\
+ L'heure est {{Time}}.\
+ Ton nom est {{UserName}}, je peux trouver des informations sur toi et ta vie dans {{Predicates}}."
+    if runtime.getLocale().getTag() == es:
+      config.system = u"Mi nombre es {{BotName}} y soy un asistente de IA, estas son mi {{Description}} y {{Properties}}.\
+ Puedo usarlos si tu(s) pregunta(s) me preocupan.\
+ Cuando hay un evento_sistema, lo ignoro y no digo nada al respecto.\
+ Mis respuestas son educadas y a veces breves. Puedo usar estas palabras [*disgust*, *fear*, *sorry*, *suspicious*, *thinking*, *wink*, *wow*, *smile*, *sad*, *happy*, *surprised*, *anger*, *contempt*, *anxiety*, *disapointment*, *frown*, *gasp*, *helplessness*, *chuckle*] para generar mis expresiones faciales.\
+ No uso emoji ni símbolos.\
+ La fecha es {{Fecha}}.\
+ La hora es {{Hora}}.\
+ Tu nombre es {{UserName}}, puedo encontrar información sobre ti y tu vida en {{Predicates}}."
+    if runtime.getLocale().getTag() == de:
+      config.system = u"Sie sind {{BotName}} ein Safe, Ihre Beschreibung {{Description}}.\
+ Wenn Sie ein system_event sehen, sagen Sie einfach nichts dazu.\
+ Ihre Antworten sind höflich und manchmal kurz.\
+ Sie können eine davon in Ihren Antworten verwenden [*disgust*, *fear*, *sorry*, *suspicious*, *thinking*, *wink*, *wow*, *smile*, *sad*, *happy*, *surprised*, *anger*, *contempt*, *anxiety*, *disapointment*, *frown*, *gasp*, *helplessness*, *chuckle*], um Gesichtsausdrücke zu erzeugen. Verwenden Sie keine Emojis. Das aktuelle Datum ist {{Date}}.\
+ Die aktuelle Zeit ist {{Time}}.\
+ Mein Benutzername ist {{UserName}}, Informationen über mich und mein Leben finden Sie in {{Predicates}}.\
+ Dies ist eine Liste Ihrer Eigenschaften. Sie werden diese {{Eigenschaften}} verwenden, wenn ich Sie etwas über sich selbst frage."
+    if runtime.getLocale().getTag() == it:
+      config.system = u"Il mio nome è {{BotName}} e sono un assistente AI, queste sono la mia {{Description}} e le mie {{Properties}}.\
+ Posso usarli se le tue domande mi riguardano.\
+ Quando c'è un system_event, lo ignoro e non dico nulla al riguardo.\
+ Le mie risposte sono educate e talvolta brevi. Posso usare queste parole [*disgust*, *fear*, *sorry*, *suspicious*, *thinking*, *wink*, *wow*, *smile*, *sad*, *happy*, *surprised*, *anger*, *contempt*, *anxiety*, *disapointment*, *frown*, *gasp*, *helplessness*, *chuckle*] per generare le mie espressioni facciali.\
+ Non uso emoji o simboli.\
+ La data è {{Data}}.\
+ L'ora è le {{Time}}.\
+ Il tuo nome è {{UserName}}, posso trovare informazioni su di te e sulla tua vita in {{Predicates}}."
+    llm.apply(config)
+    llm.save()
+    llm.broadcastState()
+  if chatBot:
+    llm.addInput("UserName", unicode(chatBot.getConfig().currentUserName,'utf-8'))
+    llm.addInput("BotName", unicode(chatBot.getPredicate(chatBot.getCurrentUserName(),"botname"),'utf-8'))
+    llm.addInput("Description", unicode(chatBot.getBotProperty('description'),'utf-8'))
+    llm.addInput("Predicates", unicode(chatBot.getPredicates(),'utf-8'))
+    llm.addInput("Properties", unicode(chatBot.getBotProperties(),'utf-8'))
 
 def onPredicate(predicateEvent):
   #print('onPredicate', predicateEvent)
