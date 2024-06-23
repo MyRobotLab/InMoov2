9009ba57b97fee2d65531ca433a6ac96514b1a3e
Langevin Gael <hairygael@gmail.com>
2024-06-23 23:32:30 +0200

    Update No.py

--- gestures/No.py
+++ gestures/No.py
@@ -1,11 +1,8 @@
 def No():
   if runtime.isStarted('i01.head'):
-    i01.setHeadSpeed(45,45,45)
-    i01.moveHeadBlocking(80,130)
-    sleep(0.5)
-    i01.moveHeadBlocking(80,50)
-    sleep(0.5)
-    i01.moveHeadBlocking(80,130)
-    sleep(0.5)
-    i01.moveHeadBlocking(80,90)
+    i01_head_rothead.setSpeed(90)
+    i01_head_rothead.moveToBlocking(130)
+    i01_head_rothead.moveToBlocking(50)
+    i01_head_rothead.moveToBlocking(130)
+    i01_head_rothead.rest()
     i01.finishedGesture()
