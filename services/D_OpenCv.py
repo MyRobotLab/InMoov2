b461f04e291e2519e28b27bf44a95e8b400bc43e
Langevin Gael <hairygael@gmail.com>
2024-06-27 15:50:54 +0200

    Update D_OpenCv.py

--- services/D_OpenCv.py
+++ services/D_OpenCv.py
@@ -7,17 +7,18 @@
 #                 SET SERVICE
 # ##############################################################################
 
-#i01_opencv = Runtime.create("i01.opencv", "OpenCV")
-opencv = runtime.getService('i01.opencv')
 def initOpencv():
+  opencv = runtime.getService("i01.opencv")
   opencv.getConfig()
   python.subscribe("opencv", "publishRecognizedFace")
 
 def initTracking():
-  if runtime.isStarted('i01.headTracking'):
-    i01_headTracking.getConfig()
-  if runtime.isStarted('i01.eyeTracking'):
-    i01_eyeTracking.getConfig()
+  headTracking = runtime.getService("i01.headTracking")
+  if headTracking:
+    headTracking.getConfig()
+  eyeTracking = runtime.getService("i01.eyeTracking")
+  if eyeTracking:
+    eyeTracking.getConfig()
   trackingTimer = runtime.start("trackingTimer","Clock")
   trackingTimer.addListener("pulse", python.name, "trackingTimerRoutine")
   trackingTimer.setInterval(i01.getConfig().trackingTimeoutMs)  
@@ -26,14 +27,17 @@ def initTracking():
 def onRecognizedFace(name):
   #print(name)
   # robot reaction if recognized face ( todo beter reaction... )
-  if runtime.isStarted('i01.opencv'):
-    if runtime.isStarted('i01.chatBot'):
-      i01_chatBot.startSession(unicode(name,'utf-8'))
-      opencv.removeFilter("FaceRecognizer")
-      i01_chatBot.getResponse("SYSTEM_SAY_HELLO")
-      opencv.stopCapture()
+  opencv = runtime.getService("i01.opencv")
+  chatBot = runtime.getService("i01.chatBot")
+  if opencv:
+    opencv.removeFilter("FaceRecognizer")
+    sleep(1)
+    opencv.stopCapture()
+    if chatBot:
+      chatBot.startSession(unicode(name,'utf-8'))
+      chatBot.getResponse("SYSTEM_SAY_HELLO")
       print("recognized:" + name)
   else:
-    if runtime.isStarted('i01.chatBot'):
-      i01_chatBot.getResponse("ALERT")
-      i01_chatBot.getResponse("SYSTEM_ERROR_OPENCV_1")
+    if chatBot:
+      chatBot.getResponse("ALERT")
+      chatBot.getResponse("SYSTEM_ERROR_OPENCV_1")
