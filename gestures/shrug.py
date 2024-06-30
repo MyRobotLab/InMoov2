2de4d022c37c6e821740427bd601dccdba5103ac
Langevin Gael <hairygael@gmail.com>
2024-06-30 11:08:13 +0200

    Update shrug.py

--- gestures/shrug.py
+++ gestures/shrug.py
@@ -1,5 +1,21 @@
 def shrug():
-  sleep(1)
+  ## i2Head
+  i01_head_forheadLeft.moveTo(0.0)
+  i01_head_forheadRight.moveTo(0.0)
+  i01_head_upperLip.moveTo(113.0)
+  i01_head_cheekLeft.moveTo(82.0)
+  i01_head_cheekRight.moveTo(180.0)
+  i01_head_eyebrowLeft.moveTo(76.0)
+  i01_head_eyebrowRight.moveTo(29.0)
+  i01_head_eyelidRightLower.moveTo(23.0)
+  i01_head_eyelidRightUpper.moveTo(100.0)
+  i01_head_eyelidLeftLower.moveTo(72.0)
+  i01_head_eyelidLeftUpper.moveTo(137.0)
+  i01_head_eyeRightLR.moveTo(84.0)
+  i01_head_eyeRightUD.moveTo(90.0)
+  i01_head_eyeLeftLR.moveTo(88.0)
+  i01_head_eyeLeftUD.moveTo(84.0)
+  ##body
   i01.setHandSpeed("left", 19, 19, 19, 19, 19, 19)
   i01.setHandSpeed("right", 19, 36, 19, 19, 19, 19)
   i01.setArmSpeed("left", 60, 60, 60, 60)
@@ -12,4 +28,5 @@ def shrug():
   i01.moveHand("right",2,2,2,2,2,90)
   sleep(1)
   i01.finishedGesture()
+  neutral()
   relax()
