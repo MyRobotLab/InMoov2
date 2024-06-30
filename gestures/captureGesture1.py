2b67623e181a7c4f8d025bdb8c8bd8e5467631a5
Langevin Gael <hairygael@gmail.com>
2024-06-30 11:01:59 +0200

    Update captureGesture1.py

--- gestures/captureGesture1.py
+++ gestures/captureGesture1.py
@@ -1,4 +1,25 @@
 def captureGesture1():
+  print"i01_head_eyeLeftUD.moveTo("+str(i01_head_eyeLeftUD.getPos())+")"
+  print"i01_head_eyeLeftLR.moveTo("+str(i01_head_eyeLeftLR.getPos())+")"
+  print"i01_head_eyeRightUD.moveTo("+str(i01_head_eyeRightUD.getPos())+")"
+  print"i01_head_eyeRightLR.moveTo("+str(i01_head_eyeRightLR.getPos())+")"
+  ## the eyelids
+  print"i01_head_eyelidLeftUpper.moveTo("+str(i01_head_eyelidLeftUpper.getPos())+")"
+  print"i01_head_eyelidLeftLower.moveTo("+str(i01_head_eyelidLeftLower.getPos())+")"
+  print"i01_head_eyelidRightUpper.moveTo("+str(i01_head_eyelidRightUpper.getPos())+")"
+  print"i01_head_eyelidRightLower.moveTo("+str(i01_head_eyelidRightLower.getPos())+")"
+  ## the eyebrows
+  print"i01_head_eyebrowRight.moveTo("+str(i01_head_eyebrowRight.getPos())+")"
+  print"i01_head_eyebrowLeft.moveTo("+str(i01_head_eyebrowLeft.getPos())+")"
+  ## the cheeks
+  print"i01_head_cheekRight.moveTo("+str(i01_head_cheekRight.getPos())+")"
+  print"i01_head_cheekLeft.moveTo("+str(i01_head_cheekLeft.getPos())+")"
+  ## the upper lip
+  print"i01_head_upperLip.moveTo("+str(i01_head_upperLip.getPos())+")"
+  ## the for head
+  print"i01_head_forheadRight.moveTo("+str(i01_head_forheadRight.getPos())+")"
+  print"i01_head_forheadLeft.moveTo("+str(i01_head_forheadLeft.getPos())+")"
+  print"## i2Head"
   #head
   print"i01_head.moveTo("+str(i01_head_neck.getPos())+","+str(i01_head_rothead.getPos())+","+str(i01_head_eyeX.getPos())+","+str(i01_head_eyeY.getPos())+","+str(i01_head_jaw.getPos())+","+str(i01_head_rollNeck.getPos())+")"
   #armLeft
