19df1501fac1a1b0b926dd978ac82b5f5159ce14
Langevin Gael <hairygael@gmail.com>
2024-11-10 21:24:08 +0100

    Add files via upload

--- services/C_Pir.py
+++ services/C_Pir.py
@@ -7,39 +7,52 @@
 #                 SET SERVICE
 # ##############################################################################
 
-#publishPin=0
 def initPir():
   pir=i01_pir
   pir.getConfig()
   #pirPin=pir.getConfig().pin
   #pirControlerArduino=pir.getConfig().controller
-  pir.addListener("publishSense",python.name,"publishSense")
+  #pir.addListener("publishSense",python.name,"publishSense")
+  pir.addListener("publishPirOn",python.name,"publishPirOn")
   sleepTimer = runtime.start("sleepTimer","Clock")
   sleepTimer.addListener("pulse", python.name, "sleepTimerRoutine")
   sleepTimer.setInterval(i01.getConfig().sleepTimeoutMs)
 
+# Deprecated
+# analog pin listener read the pir
+# def publishSense(event):
+#   if runtime.isStarted("i01.pir"):
+#     #publishPin=1
+#     if event:
+#       print "pir detected something"
+#       #if not i01.RobotIsSleeping and i01.RobotIsStarted:
+#       if runtime.isStarted('i01.fsm'):
+#         if i01_fsm.getState()=="idle" or "setup" or "wake":
+#           #i01_pir.disable()
+#           humanDetected()
+#         #wakeup action
+#         if i01_fsm.getState()=="sleep":
+#           i01_pir.disable()
+#           sleepModeWakeUp()
+#       else:
+#         i01_pir.disable()
+#         sleepModeWakeUp()
 
-#analog pin listener read the pir
-def publishSense(event):
+def publishPirOn():
   if runtime.isStarted("i01.pir"):
-    #publishPin=1
-    if event:
-      print "pir detected something"
-      #if not i01.RobotIsSleeping and i01.RobotIsStarted:
-      if runtime.isStarted('i01.fsm'):
-        if i01_fsm.getState()=="idle" or "setup" or "wake":
-          #i01_pir.disable()
-          humanDetected()
-        #wakeup action
-        if i01_fsm.getState()=="sleep":
-          i01_pir.disable()
-          sleepModeWakeUp()
-      else:
+    print "pir detected something"
+    if runtime.isStarted('i01.fsm'):
+      if i01_fsm.getState()=="idle" or "setup" or "wake":
+        humanDetected()
+      #wakeup action
+      if i01_fsm.getState()=="sleep":
         i01_pir.disable()
         sleepModeWakeUp()
+    else:
+      i01_pir.disable()
+      sleepModeWakeUp()
           
   else:
     if runtime.isStarted('i01.chatBot'):
       i01_chatBot.getResponse("ALERT")
       i01_chatBot.getResponse("SYSTEM_ERROR_PIR_1")
-    
