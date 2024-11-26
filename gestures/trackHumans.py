744e50a436636cf4010d87312a73c309d843a1c4
Langevin Gael <hairygael@gmail.com>
2024-11-26 22:07:50 +0100

    minor change rollNeck

--- gestures/trackHumans.py
+++ gestures/trackHumans.py
@@ -4,7 +4,8 @@ def trackHumans():
       i01_fsm.fire("track")
     if runtime.isStarted('i01.head'):  
       i01_head_rollNeck.setAutoDisable(False)
-      i01_head_rollNeck.moveToBlocking(90)
+      i01_head_rollNeck.enable()
+      i01_head_rollNeck.moveTo(90)
       i01.setHeadSpeed(500.0,500.0,500.0,500.0,500.0)
     i01_opencv.removeFilters()
     i01.startPeer("headTracking")
