e76b0a03b619881ba02207331a39228e03134967
Langevin Gael <hairygael@gmail.com>
2024-12-09 21:47:22 +0100

    Add files via upload

---
+++ gestures/startRadio.py
@@ -0,0 +1,24 @@
+def startRadio(station):
+    if runtime.isStarted("i01.audioPlayer"):
+        audioPlayer = runtime.getService("i01.audioPlayer")
+        audiocfg = audioPlayer.getConfig()
+        print("Radio started")
+        if audiocfg.stream == 0:
+            audiocfg.stream = 1
+            audioPlayer.save()
+            audioPlayer.apply(audiocfg)
+            audioPlayer.broadcastState()
+        print(station)
+        audioPlayer.playFile(station)
+
+def stopRadio():
+    if runtime.isStarted('i01.audioPlayer'):
+        audioPlayer = runtime.getService('i01.audioPlayer')
+        audioPlayer.stop()
+        print("Radio stopped")
+        audiocfg = audioPlayer.getConfig()
+        if audiocfg.stream == 1:
+            audiocfg.stream = 1
+            audioPlayer.save()
+            audioPlayer.apply(audiocfg)
+            audioPlayer.broadcastState()
