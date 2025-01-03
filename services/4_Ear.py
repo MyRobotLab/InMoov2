126c50534a8540d50c0d57db26aced6e7c460987
Langevin Gael <hairygael@gmail.com>
2025-01-03 23:14:17 +0100

    setWakeWord multilangue

--- services/4_Ear.py
+++ services/4_Ear.py
@@ -1,3 +1,4 @@
+# -- coding: utf-8 --
 # ##############################################################################
 #                 EAR SERVICE FILE
 # ##############################################################################  
@@ -6,13 +7,47 @@
 # ##############################################################################
 #                SET SERVICE
 # ##############################################################################
-
+en = 'en-US'
+fi = 'fi-FI'
+fr = 'fr-FR'
+hi = 'hi-IN'
+es = 'es-ES'
+de = 'de-DE'
+it = 'it-IT'
+nl = 'nl-NL'
+pl = 'pl-PL'
+pt = 'pt-PT'
+ru = 'ru-RU'
+tr = 'tr-TR'
 def initEar():
   i01_ear.getConfig()
   lockPhrase = i01_ear.getConfig().wakeWord
   i01_ear.setWakeWordTimeout(5000)
   if lockPhrase==None:
-    i01_ear.setWakeWord('wake up')
+    if runtime.getLocale().getTag() == en:
+      i01_ear.setWakeWord(u'wake up')
+    if runtime.getLocale().getTag() == fi:
+      i01_ear.setWakeWord(u'herää')
+    if runtime.getLocale().getTag() == fr:
+      i01_ear.setWakeWord(u'réveille toi')
+    if runtime.getLocale().getTag() == hi:
+      i01_ear.setWakeWord(u'उठो')
+    if runtime.getLocale().getTag() == es:
+      i01_ear.setWakeWord(u'despiértate')
+    if runtime.getLocale().getTag() == de:
+      i01_ear.setWakeWord(u'aufwachen')
+    if runtime.getLocale().getTag() == it:
+      i01_ear.setWakeWord(u'svegliati')
+    if runtime.getLocale().getTag() == nl:
+      i01_ear.setWakeWord(u'waker worden')
+    if runtime.getLocale().getTag() == pl:
+      i01_ear.setWakeWord(u'budzić się')
+    if runtime.getLocale().getTag() == pt:
+      i01_ear.setWakeWord(u'acorde')
+    if runtime.getLocale().getTag() == ru:
+      i01_ear.setWakeWord(u'просыпайся')
+    if runtime.getLocale().getTag() == tr:
+      i01_ear.setWakeWord(u'uyanmak')
   python.subscribe(i01_ear.getName(),"publishRecognized")  
 
 #if runtime.isStarted('i01.ear'):
@@ -55,15 +90,30 @@ if i01.getConfig().forceMicroOnIfSleeping==0:
   runtime.release("watchMicro")  
 
 def earON():
+  if runtime.isStarted('i01.ear'):
+    i01_ear.setAwake(True)
+    i01_ear.startRecording()
+  else:  
     ear = runtime.start("i01.ear","WebkitSpeechRecognition")
-    # this needs the library to be loaded and may not work in Linux, Mac
-    subprocess.call(["data/F5.exe"])
     ear.startListening()
+  if i01.getConfig().forceMicroOnIfSleeping==1:
+    if runtime.isStarted('watchMicro'):
+      pass
+    else:
+      watchMicro = runtime.start("watchMicro","Clock")
+      # set our watchMicro to fire if its not reset within a second
+      watchMicro.setInterval(60000) 
+      watchMicro.addClockEvent("python","exec", "restartListening()")
+      # start the clock
+      watchMicro.startClock()
 
     
 def earOFF():
-    runtime.release("i01.ear")
-    i01.invoke("publishEvent", "STOPPED EAR")
+  initEar()
+  if runtime.isStarted('watchMicro'):
+    runtime.release('watchMicro')
+  if runtime.isStarted('i01.ear'):
+    i01_ear.setAwake(False)
 
 
 # define the method that will restart the listening
