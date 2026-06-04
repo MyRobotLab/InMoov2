2c28409790726767fcd3adbbf7e31003d27f5aaa
Langevin Gael <hairygael@gmail.com>
2026-06-04 10:30:48 +0200

    Enhance ultrasonic function with delays and logging
    Added sleep delay and print statements for ultrasonic distances.

--- gestures/ultraSonic.py
+++ gestures/ultraSonic.py
@@ -1,2 +1,7 @@
 def ultrasonic(returnText):
-  i01.speakBlocking(unicode(returnText,'utf-8')+str(i01.getUltrasonicRightDistance()).replace(".0", "")+str(i01.getUltrasonicLeftDistance()).replace(".0", ""))
+  i01.speakBlocking(unicode(returnText,'utf-8')+str(i01.getUltrasonicRightDistance()).replace(".0", ""))
+  sleep(0.5)
+  i01.speakBlocking(unicode(returnText,'utf-8')+str(i01.getUltrasonicLeftDistance()).replace(".0", ""))
+  print("right side:"+ str(i01.getUltrasonicRightDistance()).replace(".0", "")+ "cm")
+  print("left side:"+ str(i01.getUltrasonicLeftDistance()).replace(".0", "")+ "cm")
+  
