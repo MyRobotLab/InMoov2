17547e34bb1825427537c457430e7a6e410a4d8b
Langevin Gael <hairygael@gmail.com>
2026-06-04 10:31:57 +0200

    Log left ultrasonic distance in ultrasonicLeft function
    Added print statement to log left ultrasonic distance.

--- gestures/ultraSonicLeft.py
+++ gestures/ultraSonicLeft.py
@@ -1,2 +1,3 @@
 def ultrasonicLeft(returnText):
   i01.speakBlocking(unicode(returnText,'utf-8')+str(i01.getUltrasonicLeftDistance()).replace(".0", ""))
+  print("left side:"+ str(i01.getUltrasonicLeftDistance()).replace(".0", "")+ "cm")
