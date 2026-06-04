205b7d9f01f738e720b5786902e2eae1083f8cd2
Langevin Gael <hairygael@gmail.com>
2026-06-04 10:32:42 +0200

    Log ultrasonic distance on right side
    Added print statement to display right ultrasonic distance.

--- gestures/ultraSonicRight.py
+++ gestures/ultraSonicRight.py
@@ -1,2 +1,3 @@
 def ultrasonicRight(returnText):
   i01.speakBlocking(unicode(returnText,'utf-8')+str(i01.getUltrasonicRightDistance()).replace(".0", ""))
+  print("right side:"+ str(i01.getUltrasonicRightDistance()).replace(".0", "")+ "cm")
